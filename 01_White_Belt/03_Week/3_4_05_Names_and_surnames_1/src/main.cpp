3bdb1e2302bfc5fe1961e431c9e629d239249634
pavelzep <pavelzep@gmail.com>
2023-01-18 10:15:02 +0500

    finished 3_4_05_Names_and_surnames_1t

--- 01_White_Belt/03_Week/3_4_05_Names_and_surnames_1/src/main.cpp
+++ 01_White_Belt/03_Week/3_4_05_Names_and_surnames_1/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <map>
-
+#include <vector>
+#include <set> 
 using namespace std;
 
 class Person {
@@ -8,27 +9,57 @@ public:
 	void ChangeFirstName(int year, const string &first_name) {
 		// добавить факт изменения имени на first_name в год year
 		firstNames[year]=first_name;
+
 	}
 	void ChangeLastName(int year, const string &last_name) {
 		// добавить факт изменения фамилии на last_name в год year
 		lastNames[year]=last_name;
 	}
+
+	string GetFirstName(int year){
+		if(firstNames.count(year)){
+			return firstNames[year];	
+		}
+			else{
+
+			}
+	}
+
+
+
 	string GetFullName(int year) {
 		// получить имя и фамилию по состоянию на конец года year
 		string rezult;
 
-		bool first_name_valid = firstNames.count(year);
-		bool last_name_valid = lastNames.count(year);
+		int first_name_year = 0;
+		int last_name_year = 0;
+
+		for(const auto & item :firstNames){
+			if(item.first <= year){
+				first_name_year=item.first;
+			}
+		}
+
+		for(const auto & item :lastNames){
+			if(item.first <= year){
+				last_name_year=item.first;
+			}
+		}
+
+
+
+		bool first_name_valid = firstNames.count(first_name_year);
+		bool last_name_valid = lastNames.count(last_name_year);
 
 		if(first_name_valid && last_name_valid){
-			cout << firstNames[year] << ' ' << lastNames[year];
+			cout << firstNames[first_name_year] << ' ' << lastNames[last_name_year];
 
 		}else if (!first_name_valid && last_name_valid){
-			cout << lastNames[year]<< " with unknown first name";
+			cout << lastNames[last_name_year]<< " with unknown first name";
 
 		}else if(first_name_valid && !last_name_valid){
 
-			cout << firstNames[year] << " with unknown last name";
+			cout << firstNames[first_name_year] << " with unknown last name";
 		}else if(!first_name_valid && !last_name_valid){
 			cout << "Incognito" ;
 		}
@@ -36,11 +67,14 @@ public:
 		return rezult;
 	}
 
-private:
 
+
+
+private:
+	// приватные поля	
 	map<int, string> firstNames;
 	map<int, string> lastNames;
-	// приватные поля
+	
 };
 
 int main() {
