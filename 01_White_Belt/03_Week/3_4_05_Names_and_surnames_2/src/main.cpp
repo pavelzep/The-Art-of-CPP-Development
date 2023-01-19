04ebbe40f568b72b3a27605ade362e15adba00b8
pavelzep <pavelzep@gmail.com>
2023-01-19 13:10:01 +0500

    3_4_05_Names_and_surnames_2 finished

--- 01_White_Belt/03_Week/3_4_05_Names_and_surnames_2/src/main.cpp
+++ 01_White_Belt/03_Week/3_4_05_Names_and_surnames_2/src/main.cpp
@@ -8,93 +8,165 @@ class Person {
 public:
 	void ChangeFirstName(int year, const string &first_name) {
 		// добавить факт изменения имени на first_name в год year
-		firstNames[year]=first_name;
-
+		firstNames[year] = first_name;
 	}
 	void ChangeLastName(int year, const string &last_name) {
 		// добавить факт изменения фамилии на last_name в год year
-		lastNames[year]=last_name;
+		lastNames[year] = last_name;
 	}
-
-	string GetFirstName(int year){
-		if(firstNames.count(year)){
-			return firstNames[year];	
-		}
-			else{
-
-			}
-	}
-
-
 
 	string GetFullName(int year) {
 		// получить имя и фамилию по состоянию на конец года year
 		string rezult;
 
-		int first_name_year = 0;
-		int last_name_year = 0;
+		string first_name = getNameForYear(year, firstNames);
+		string last_name = getNameForYear(year, lastNames);
 
-		for(const auto & item :firstNames){
-			if(item.first <= year){
-				first_name_year=item.first;
-			}
-		}
+		if (!first_name.empty() && !last_name.empty()) {
+			rezult = first_name + " " + last_name;
 
-		for(const auto & item :lastNames){
-			if(item.first <= year){
-				last_name_year=item.first;
-			}
-		}
+		} else if (first_name.empty() && !last_name.empty()) {
+			rezult = last_name + " with unknown first name";
 
-
-
-		bool first_name_valid = firstNames.count(first_name_year);
-		bool last_name_valid = lastNames.count(last_name_year);
-
-		if(first_name_valid && last_name_valid){
-			cout << firstNames[first_name_year] << ' ' << lastNames[last_name_year];
-
-		}else if (!first_name_valid && last_name_valid){
-			cout << lastNames[last_name_year]<< " with unknown first name";
-
-		}else if(first_name_valid && !last_name_valid){
-
-			cout << firstNames[first_name_year] << " with unknown last name";
-		}else if(!first_name_valid && !last_name_valid){
-			cout << "Incognito" ;
+		} else if (!first_name.empty() && last_name.empty()) {
+			rezult = first_name + " with unknown last name";
+		} else if (first_name.empty() && last_name.empty()) {
+			rezult = "Incognito";
 		}
 
 		return rezult;
 	}
 
+	string GetFullNameWithHistory(int year) {
+		// получить все имена и фамилии по состоянию на конец года year
+		string rezult;
 
+		string first_name = GetNameWithHistory(year, firstNames);
+		string last_name = GetNameWithHistory(year, lastNames);
 
+		if (!first_name.empty() && !last_name.empty()) {
+			rezult = first_name + " " + last_name;
+
+		} else if (first_name.empty() && !last_name.empty()) {
+			rezult = last_name + " with unknown first name";
+
+		} else if (!first_name.empty() && last_name.empty()) {
+			rezult = first_name + " with unknown last name";
+		} else if (first_name.empty() && last_name.empty()) {
+			rezult = "Incognito";
+		}
+
+		return rezult;
+
+	}
 
 private:
 	// приватные поля	
 	map<int, string> firstNames;
 	map<int, string> lastNames;
-	
-};
+
+	string GetNameWithHistory(int _year, map<int, string> _names) {
+		string rezult;
+		vector<string> names;
+
+		for (const auto &item : _names) {
+			if (item.first <= _year) {
+				names.push_back(item.second);
+				if (names[names.size() - 1] == names[names.size() - 2]) {
+					names.pop_back();
+				}
+			}
+		}
+
+		if (names.size()) {
+
+			//int d1 = names.size();////
+			rezult = names[names.size() - 1];
+			names.pop_back();
+			string prevNames;
+
+			if (names.size()) {
+
+				rezult += " (";
+
+				while (names.size()) {
+					prevNames += names[names.size() - 1];
+					names.pop_back();
+					if (names.size()) {
+						prevNames += ", ";
+					}
+				}
+				rezult += prevNames;
+				rezult += ')';
+			}
+
+		}
+
+		return rezult;
+	}
+
+	string getNameForYear(int _year, map<int, string> _names) {
+		int __year;
+		for (const auto &item : _names) {
+			if (item.first <= _year) {
+				__year = item.first;
+			}
+		}
+		return _names[__year];
+	}
+}
+;
 
 int main() {
 	Person person;
+	/*
+	 person.ChangeFirstName(1965, "Polina");
+	 person.ChangeLastName(1967, "Sergeeva");
+	 for (int year : { 1900, 1965, 1990 }) {
+	 cout << person.GetFullName(year) << endl;
+	 }
+
+	 person.ChangeFirstName(1970, "Appolinaria");
+	 for (int year : { 1969, 1970 }) {
+	 cout << person.GetFullName(year) << endl;
+	 }
+
+	 person.ChangeLastName(1968, "Volkova");
+	 for (int year : { 1969, 1970 }) {
+	 cout << person.GetFullName(year) << endl;
+	 }
+	 */
 
 	person.ChangeFirstName(1965, "Polina");
 	person.ChangeLastName(1967, "Sergeeva");
 	for (int year : { 1900, 1965, 1990 }) {
-		cout << person.GetFullName(year) << endl;
+		cout << person.GetFullNameWithHistory(year) << endl;
 	}
 
 	person.ChangeFirstName(1970, "Appolinaria");
 	for (int year : { 1969, 1970 }) {
-		cout << person.GetFullName(year) << endl;
+		cout << person.GetFullNameWithHistory(year) << endl;
 	}
 
 	person.ChangeLastName(1968, "Volkova");
 	for (int year : { 1969, 1970 }) {
-		cout << person.GetFullName(year) << endl;
+		cout << person.GetFullNameWithHistory(year) << endl;
 	}
+
+	person.ChangeFirstName(1990, "Polina");
+	person.ChangeLastName(1990, "Volkova-Sergeeva");
+	cout << person.GetFullNameWithHistory(1990) << endl;
+
+	person.ChangeFirstName(1966, "Pauline");
+	cout << person.GetFullNameWithHistory(1966) << endl;
+
+	person.ChangeLastName(1960, "Sergeeva");
+	for (int year : { 1960, 1967 }) {
+		cout << person.GetFullNameWithHistory(year) << endl;
+	}
+
+	person.ChangeLastName(1961, "Ivanova");
+	cout << person.GetFullNameWithHistory(1967) << endl;
 
 	return 0;
 }
