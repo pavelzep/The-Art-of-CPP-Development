e2812766a523dbd10803b2a9887e274b734439d5
pavelzep <pavelzep@gmail.com>
2023-01-20 15:35:56 +0500

    3_6_06_string finished

--- 01_White_Belt/03_Week/3_6_06_String/src/main.cpp
+++ 01_White_Belt/03_Week/3_6_06_String/src/main.cpp
@@ -1,19 +1,43 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
 
 
+using namespace std;
 
+class ReversibleString {
+public:
+	ReversibleString() {
+	}
+
+	ReversibleString(const string &_str) {
+		str = _str;
+	}
+
+	void Reverse() {
+		reverse(str.begin(),str.end());
+	}
+
+	string ToString() const {
+		return str;
+	}
+
+private:
+	string str;
+};
 
 int main() {
-  ReversibleString s("live");
-  s.Reverse();
-  cout << s.ToString() << endl;
-  
-  s.Reverse();
-  const ReversibleString& s_ref = s;
-  string tmp = s_ref.ToString();
-  cout << tmp << endl;
-  
-  ReversibleString empty;
-  cout << '"' << empty.ToString() << '"' << endl;
-  
-  return 0;
+	ReversibleString s("live");
+	s.Reverse();
+	cout << s.ToString() << endl;
+
+	s.Reverse();
+	const ReversibleString &s_ref = s;
+	string tmp = s_ref.ToString();
+	cout << tmp << endl;
+
+	ReversibleString empty;
+	cout << '"' << empty.ToString() << '"' << endl;
+
+	return 0;
 }
