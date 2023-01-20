0ab5c39657c90a50d389c40ed1d7ec86eac9f504
pavezep <pavelzep@gmail.com>
2023-01-20 23:58:58 +0500

    3_6_08_Ways_to_initialize finished

--- 01_White_Belt/03_Week/3_6_08_Ways_to_initialize/src/main.cpp
+++ 01_White_Belt/03_Week/3_6_08_Ways_to_initialize/src/main.cpp
@@ -1,1 +1,27 @@
-int main(){}
+class Incognizable {
+public:
+	Incognizable() {
+		i={};
+		j={};
+	}
+
+	Incognizable(int _i) {
+		i = _i;
+		j={};
+	}
+	Incognizable(int _i, int _j) {
+		i=_i;
+		j=_j;
+	}
+private:
+	int i, j;
+
+};
+
+int main() {
+	Incognizable a;
+	Incognizable b = { };
+	Incognizable c = { 0 };
+	Incognizable d = { 0, 1 };
+	return 0;
+}
