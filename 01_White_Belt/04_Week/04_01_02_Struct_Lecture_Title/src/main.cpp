ecb83142c6ba4ad363d22354b1c84cd408a3b644
pavezep <pavelzep@gmail.com>
2023-01-21 23:06:49 +0500

    04_01_02_Struct_Lecture_Title finished

--- 01_White_Belt/04_Week/04_01_02_Struct_Lecture_Title/src/main.cpp
+++ 01_White_Belt/04_Week/04_01_02_Struct_Lecture_Title/src/main.cpp
@@ -1,10 +1,36 @@
 #include <string>
 using namespace std;
 
+struct Specialization {
+	string data;
+	explicit Specialization(string new_data) {
+		data = new_data;
+	}
+};
+struct Course {
+	string data;
+	explicit Course(string new_data) {
+		data = new_data;
+	}
+};
+
+struct Week {
+	string data;
+	explicit Week(string new_data) {
+		data = new_data;
+	}
+};
+
 struct LectureTitle {
 	string specialization;
-  string course;
-  string week;
+	string course;
+	string week;
+	LectureTitle(Specialization new_specialization, Course new_course, Week new_week){
+		specialization = new_specialization.data;
+		course = new_course.data;
+		week =  new_week.data;
+		;
+	}
 };
 
 int main() {
