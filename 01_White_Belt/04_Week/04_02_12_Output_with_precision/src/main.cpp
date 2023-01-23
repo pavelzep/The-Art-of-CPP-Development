44d9ef9fa8ef895d54b907c71ab8ca0e0ccc81a3
pavezep <pavelzep@gmail.com>
2023-01-23 22:55:59 +0500

    04_02_12_Output_with_precision/src/main.cpp finished

--- 01_White_Belt/04_Week/04_02_12_Output_with_precision/src/main.cpp
+++ 01_White_Belt/04_Week/04_02_12_Output_with_precision/src/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
 	ifstream input("input.txt");
+
 	string line;
 	if (input) {
 		while (getline(input, line)) {
-			cout << line << endl;
+			cout << fixed << setprecision(3) << stod(line)<< endl;
 		}
 	}
 	return 0;
