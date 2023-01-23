c04137cb79a9c03bdcf23cf6481daf3b9d624b9a
pavelzep <pavelzep@gmail.com>
2023-01-23 11:55:59 +0500

    04_02_10_Working_with_files_1 finished

--- 01_White_Belt/04_Week/04_02_10_Working_with_files_1/src/main.cpp
+++ 01_White_Belt/04_Week/04_02_10_Working_with_files_1/src/main.cpp
@@ -1,3 +1,15 @@
+#include <iostream>
+#include <fstream>
 
+using namespace std;
 
-int main(){return 0;}
+int main() {
+	ifstream input("input.txt");
+	string line;
+	if (input) {
+		while (getline(input, line)) {
+			cout << line << endl;
+		}
+	}
+	return 0;
+}
