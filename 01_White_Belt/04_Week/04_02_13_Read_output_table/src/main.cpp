bd0c5940ea17f159a43c536670ee64c858d908fb
pavezep <pavelzep@gmail.com>
2023-01-24 21:05:36 +0500

    04_02_13_Read_output_table finished

--- 01_White_Belt/04_Week/04_02_13_Read_output_table/src/main.cpp
+++ 01_White_Belt/04_Week/04_02_13_Read_output_table/src/main.cpp
@@ -8,36 +8,35 @@ using namespace std;
 
 int main() {
 	ifstream input("input.txt");
-	ofstream output("output.txt");
 
 	int columns;
 	int rows;
-		
+
 	vector<vector<int>> data;
 	if (input) {
 		input >> rows >> columns;
-		for(int j=0; j< rows ; ++j){
+		for (int j = 0; j < rows; ++j) {
 			vector<int> temp;
 			int val;
-			for(int i =0; i< columns ;++i){
-			input >> val;
-			input.ignore(1);
-			temp.push_back(val);
+			for (int i = 0; i < columns; ++i) {
+				input >> val;
+				input.ignore(1);
+				temp.push_back(val);
 			}
 			data.push_back(temp);
-	
-		}	
-	}
-	
-	cout << rows << ' ' << columns << endl;
-	
-	for( const auto & row: data){
-		for(const auto & column: row ){
-			cout << right;
-			cout << setw(11);
-			cout << column ;
 		}
-		cout << endl;
+	}
+
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < columns; ++j) {
+
+			cout << right << setw(10);
+			cout << data.at(i).at(j);
+			if (j < columns - 1)
+				cout << ' ';
+		}
+		if (i < rows)
+			cout << endl;
 	}
 	return 0;
 }
