37f0224972461f1db0688cf31af7702e88ffa5ca
pavezep <pavelzep@gmail.com>
2023-01-24 23:18:03 +0500

    04_02_14_students_list main.cpp v1

--- 01_White_Belt/04_Week/04_02_14_Students_list/src/main.cpp
+++ 01_White_Belt/04_Week/04_02_14_Students_list/src/main.cpp
@@ -1,3 +1,38 @@
-int main(){
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Student
+{
+    string name;
+    string surname;
+    int day;
+    int month;
+    int year;
+};
+
+int main()
+{
+    int N;
+    cin >> N;
+    vector<Student> students;
+
+    while (N)
+    {
+        Student student;
+        getline(cin, student.name);
+        getline(cin, student.surname);
+        cin >> student.day >> student.month >> student.year;
+        --N;
+    }
+    int M;
+    cin >> M;
+    while (M)
+    {
+
+        --M;
+    }
+
     return 0;
 }
