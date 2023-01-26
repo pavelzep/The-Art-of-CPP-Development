c7211af8006fb6e8ac85520eb86fa676a42adcbb
pavezep <pavelzep@gmail.com>
2023-01-26 23:33:42 +0500

    04_03_04_Class_Rational_2 finished

--- 01_White_Belt/04_Week/04_03_04_Class_Rational_2/src/main.cpp
+++ 01_White_Belt/04_Week/04_03_04_Class_Rational_2/src/main.cpp
@@ -24,53 +24,6 @@ public:
             p = -p;
             q = -q;
         }
-
-        /*
-                // если числитель и знаменатель отрицательные
-                if (p < 0 && q < 0)
-                {
-                    p = -p;
-                    q = -q;
-                }
-                // если числитель отрицательный
-                else if (p < 0 && q > 0)
-                {
-                    p = -p;
-                    negativ = true;
-                }
-                // если знаменатель отрицательный
-                else if (p > 0 && q < 0)
-                {
-                    q = -q;
-                    negativ = true;
-                }
-
-                if (p == 0)
-                {
-                    q = 1;
-                }
-                else if (q % p == 0)
-                {
-                    q = q / p;
-                    p = 1;
-                }
-                else
-                {
-                    for (int i = 2; i < q / 2; i++)
-                    {
-                        while ((p % i == 0) && (q % i == 0))
-                        {
-                            p /= i;
-                            q /= i;
-                        }
-                    }
-                }
-
-                if (negativ)
-                {
-                    p = -p;
-                }
-                */
     }
 
     int Numerator() const
@@ -93,8 +46,59 @@ private:
     }
 };
 
+Rational operator+(const Rational &a, const Rational &b)
+{
+    return Rational(a.Numerator() * b.Denominator() + b.Numerator() * a.Denominator(), a.Denominator() * b.Denominator());
+}
+
+Rational operator-(const Rational &a, const Rational &b)
+{
+    return Rational(a.Numerator() * b.Denominator() - b.Numerator() * a.Denominator(), a.Denominator() * b.Denominator());
+}
+
+bool operator==(const Rational &a, const Rational &b)
+{
+    return (a.Denominator() == b.Denominator() && a.Numerator() == b.Numerator()) ? (true) : false;
+}
+
 int main()
 {
+
+    {
+        Rational r1(4, 6);
+        Rational r2(2, 3);
+        bool equal = r1 == r2;
+        if (!equal)
+        {
+            cout << "4/6 != 2/3" << endl;
+            return 1;
+        }
+    }
+
+    {
+        Rational a(2, 3);
+        Rational b(4, 3);
+        Rational c = a + b;
+        bool equal = c == Rational(2, 1);
+        if (!equal)
+        {
+            cout << "2/3 + 4/3 != 2" << endl;
+            return 2;
+        }
+    }
+
+    {
+        Rational a(5, 7);
+        Rational b(2, 9);
+        Rational c = a - b;
+        bool equal = c == Rational(31, 63);
+        if (!equal)
+        {
+            cout << "5/7 - 2/9 != 31/63" << endl;
+            return 3;
+        }
+    }
+    /*
     {
         const Rational r(3, 10);
         if (r.Numerator() != 3 || r.Denominator() != 10)
@@ -148,98 +152,99 @@ int main()
             return 5;
         }
     }
-/*
-    {
-        const Rational r(-2, -3);
-        if (r.Numerator() != 2 || r.Denominator() != 3)
+    */
+    /*
         {
-            cout << "Rational(-2, -3) != 2/3" << endl;
-            return 6;
+            const Rational r(-2, -3);
+            if (r.Numerator() != 2 || r.Denominator() != 3)
+            {
+                cout << "Rational(-2, -3) != 2/3" << endl;
+                return 6;
+            }
         }
-    }
 
-    {
+        {
 
-        int p = -2;
-        int q = 1;
-        cout << endl <<setw(5) << p << setw(5) << q << endl;
-        const Rational r(p, q);
-        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
-    }
-    {
+            int p = -2;
+            int q = 1;
+            cout << endl <<setw(5) << p << setw(5) << q << endl;
+            const Rational r(p, q);
+            cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
+        }
+        {
 
-        int p = -2;
-        int q = -3;
-        cout << setw(5) << p << setw(5) << q << endl;
-        const Rational r(p, q);
-        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
-    }
+            int p = -2;
+            int q = -3;
+            cout << setw(5) << p << setw(5) << q << endl;
+            const Rational r(p, q);
+            cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
+        }
 
-    {
+        {
 
-        int p = 1;
-        int q = -3;
-        cout << setw(5) << p << setw(5) << q << endl;
-        const Rational r(p, q);
-        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
-    }
+            int p = 1;
+            int q = -3;
+            cout << setw(5) << p << setw(5) << q << endl;
+            const Rational r(p, q);
+            cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
+        }
 
-    {
+        {
 
-        int p = 0;
-        int q = -3;
-        cout << setw(5) << p << setw(5) << q << endl;
-        const Rational r(p, q);
-        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
-    }
-    {
+            int p = 0;
+            int q = -3;
+            cout << setw(5) << p << setw(5) << q << endl;
+            const Rational r(p, q);
+            cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
+        }
+        {
 
-        int p = 0;
-        int q = 3;
-        cout << setw(5) << p << setw(5) << q << endl;
-        const Rational r(p, q);
-        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
-    }
-    {
+            int p = 0;
+            int q = 3;
+            cout << setw(5) << p << setw(5) << q << endl;
+            const Rational r(p, q);
+            cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
+        }
+        {
 
-        int p = 10;
-        int q = 2;
-        cout << setw(5) << p << setw(5) << q << endl;
-        const Rational r(p, q);
-        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
-    }
-    {
+            int p = 10;
+            int q = 2;
+            cout << setw(5) << p << setw(5) << q << endl;
+            const Rational r(p, q);
+            cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
+        }
+        {
 
-        int p = 8;
-        int q = -2;
-        cout << setw(5) << p << setw(5) << q << endl;
-        const Rational r(p, q);
-        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
-    }
+            int p = 8;
+            int q = -2;
+            cout << setw(5) << p << setw(5) << q << endl;
+            const Rational r(p, q);
+            cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
+        }
 
-    {
-        int p = 45;
-        int q = 90;
-        cout << setw(5) << p << setw(5) << q << endl;
-        const Rational r(p, q);
-        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
-    }
+        {
+            int p = 45;
+            int q = 90;
+            cout << setw(5) << p << setw(5) << q << endl;
+            const Rational r(p, q);
+            cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
+        }
 
-    {
-        int p = 24;
-        int q = 48;
-        cout << setw(5) << p << setw(5) << q << endl;
-        const Rational r(p, q);
-        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
-    }
-    {
-        int p = 11;
-        int q = 121;
-        cout << setw(5) << p << setw(5) << q << endl;
-        const Rational r(p, q);
-        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
-    }
-*/
+        {
+            int p = 24;
+            int q = 48;
+            cout << setw(5) << p << setw(5) << q << endl;
+            const Rational r(p, q);
+            cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
+        }
+        {
+            int p = 11;
+            int q = 121;
+            cout << setw(5) << p << setw(5) << q << endl;
+            const Rational r(p, q);
+            cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
+        }
+    */
     cout << "OK" << endl;
     return 0;
 }
