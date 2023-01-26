707ee5be532b46c5082214935e5062f48b372b6c
pavelzep <pavelzep@gmail.com>
2023-01-26 16:42:01 +0500

    04_03_03 main edited

--- 01_White_Belt/04_Week/04_03_03_Class_Rational/src/main.cpp
+++ 01_White_Belt/04_Week/04_03_03_Class_Rational/src/main.cpp
@@ -15,52 +15,62 @@ public:
     {
         p = numerator;
         q = denominator;
-        bool negativ = false;
 
-        // если числитель и знаменатель отрицательные
-        if (p < 0 && q < 0)
+        int gcd = GCD(p, q);
+        p /= gcd;
+        q /= gcd;
+        if (q < 0)
         {
             p = -p;
             q = -q;
         }
-        // если числитель отрицательный
-        else if (p < 0 && q > 0)
-        {
-            p = -p;
-            negativ = true;
-        }
-        // если знаменатель отрицательный
-        else if (p > 0 && q < 0)
-        {
-            q = -q;
-            negativ = true;
-        }
 
-        if (p == 0)
-        {
-            q = 1;
-        }
-        else if (q % p == 0)
-        {
-            q = q / p;
-            p = 1;
-        }
-        else
-        {
-            for (int i = 2; i < q / 2; i++)
-            {
-                while ((p % i == 0) && (q % i == 0))
+        /*
+                // если числитель и знаменатель отрицательные
+                if (p < 0 && q < 0)
                 {
-                    p /= i;
-                    q /= i;
+                    p = -p;
+                    q = -q;
                 }
-            }
-        }
+                // если числитель отрицательный
+                else if (p < 0 && q > 0)
+                {
+                    p = -p;
+                    negativ = true;
+                }
+                // если знаменатель отрицательный
+                else if (p > 0 && q < 0)
+                {
+                    q = -q;
+                    negativ = true;
+                }
 
-        if (negativ)
-        {
-            p = -p;
-        }
+                if (p == 0)
+                {
+                    q = 1;
+                }
+                else if (q % p == 0)
+                {
+                    q = q / p;
+                    p = 1;
+                }
+                else
+                {
+                    for (int i = 2; i < q / 2; i++)
+                    {
+                        while ((p % i == 0) && (q % i == 0))
+                        {
+                            p /= i;
+                            q /= i;
+                        }
+                    }
+                }
+
+                if (negativ)
+                {
+                    p = -p;
+                }
+                */
     }
 
     int Numerator() const
@@ -76,6 +86,11 @@ public:
 private:
     int p;
     int q;
+
+    int GCD(int a, int b)
+    {
+        return b ? GCD(b, a % b) : a;
+    }
 };
 
 int main()
@@ -133,74 +148,97 @@ int main()
             return 5;
         }
     }
-    /*
+
     {
-            const Rational r(-2,-3);
-            if (r.Numerator() != 2 || r.Denominator() != 3)
-            {
-                cout << "Rational(-2, -3) != 2/3" << endl;
-                return 6;
-            }
+        const Rational r(-2, -3);
+        if (r.Numerator() != 2 || r.Denominator() != 3)
+        {
+            cout << "Rational(-2, -3) != 2/3" << endl;
+            return 6;
+        }
     }
-    */
-    /*
-     {
-         const Rational r(-2, 1);
-         cout << r.Numerator() << ' ' << r.Denominator() << endl;
-     }
-     {
-         const Rational r(-2, -3);
-         cout << r.Numerator() << ' ' << r.Denominator() << endl;
-     }
 
-     {
-         const Rational r(1, -3);
-         cout << r.Numerator() << ' ' << r.Denominator() << endl;
-     }
+    {
 
-     {
-         const Rational r(0, -3);
-         cout << r.Numerator() << ' ' << r.Denominator() << endl;
-     }
-     {
-         const Rational r(0, 3);
-         cout << r.Numerator() << ' ' << r.Denominator() << endl;
-     }
-     {
-         const Rational r(10,2);
-         cout << r.Numerator() << ' ' << r.Denominator() << endl;
-     }
-     {
-         const Rational r(8, -2);
-         cout << r.Numerator() << ' ' << r.Denominator() << endl;
-     }
+        int p = -2;
+        int q = 1;
+        cout << endl <<setw(5) << p << setw(5) << q << endl;
+        const Rational r(p, q);
+        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
+    }
+    {
 
-     */
-/*
+        int p = -2;
+        int q = -3;
+        cout << setw(5) << p << setw(5) << q << endl;
+        const Rational r(p, q);
+        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
+    }
+
+    {
+
+        int p = 1;
+        int q = -3;
+        cout << setw(5) << p << setw(5) << q << endl;
+        const Rational r(p, q);
+        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
+    }
+
+    {
+
+        int p = 0;
+        int q = -3;
+        cout << setw(5) << p << setw(5) << q << endl;
+        const Rational r(p, q);
+        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
+    }
+    {
+
+        int p = 0;
+        int q = 3;
+        cout << setw(5) << p << setw(5) << q << endl;
+        const Rational r(p, q);
+        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
+    }
+    {
+
+        int p = 10;
+        int q = 2;
+        cout << setw(5) << p << setw(5) << q << endl;
+        const Rational r(p, q);
+        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
+    }
+    {
+
+        int p = 8;
+        int q = -2;
+        cout << setw(5) << p << setw(5) << q << endl;
+        const Rational r(p, q);
+        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
+    }
+
     {
         int p = 45;
         int q = 90;
         cout << setw(5) << p << setw(5) << q << endl;
         const Rational r(p, q);
-        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl;
+        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
     }
-
 
     {
         int p = 24;
         int q = 48;
         cout << setw(5) << p << setw(5) << q << endl;
         const Rational r(p, q);
-        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl;
+        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
     }
     {
         int p = 11;
         int q = 121;
         cout << setw(5) << p << setw(5) << q << endl;
         const Rational r(p, q);
-        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl;
+        cout << setw(5) << r.Numerator() << setw(5) << r.Denominator() << endl << endl;
     }
-*/
 
     cout << "OK" << endl;
     return 0;
