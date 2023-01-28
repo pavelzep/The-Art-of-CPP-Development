beb1696a7d2f7211ce9db74866c43da548345c2a
pavezep <pavelzep@gmail.com>
2023-01-28 21:47:48 +0500

    04_04_05_Function_EnsureEqual finished

--- 01_White_Belt/04_Week/04_04_05_Function_EnsureEqual/src/main.cpp
+++ 01_White_Belt/04_Week/04_04_05_Function_EnsureEqual/src/main.cpp
@@ -2,16 +2,23 @@
 #include <exception>
 using namespace std;
 
-void EnsureEqual(const string& left, const string& right){
-    
+void EnsureEqual(const string &left, const string &right)
+{
+  if (left != right)
+  {
+    throw runtime_error(left + " != " + right);
+  }
 }
 
-
-int main() {
-  try {
+int main()
+{
+  try
+  {
     EnsureEqual("C++ White", "C++ White");
     EnsureEqual("C++ White", "C++ Yellow");
-  } catch (runtime_error& e) {
+  }
+  catch (runtime_error &e)
+  {
     cout << e.what() << endl;
   }
   return 0;
