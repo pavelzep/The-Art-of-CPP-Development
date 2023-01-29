a540b862c06a43f113814b0c3171b0414a424e8f
pavezep <pavelzep@gmail.com>
2023-01-29 23:05:24 +0500

    v1

--- 01_White_Belt/05_Week/05_01_02_Final_Task/src/main.cpp
+++ 01_White_Belt/05_Week/05_01_02_Final_Task/src/main.cpp
@@ -1,28 +1,81 @@
 // Реализуйте функции и методы классов и при необходимости добавьте свои
+#include <string>
+#include <map>
+#include <iostream>
+#include <exception>
+
+using namespace std;
 
 class Date {
 public:
-  int GetYear() const;
-  int GetMonth() const;
-  int GetDay() const;
+  Date() {
+    year = 0;
+    month = 0;
+    day = 0;
+  }
+
+  Date(const int new_year, const int new_month, const int new_day) {
+    if (new_month < 1 && new_month > 12) {
+      throw exception();
+    }
+    if (new_day < 1 && new_day > 31) {
+      throw exception();
+    }
+    day = new_day;
+    month = new_month;
+    year = new_year;
+  }
+  int GetYear() const {
+    return year;
+  };
+  int GetMonth() const {
+    return month;
+  };
+  int GetDay() const {
+    return day;
+  };
+
+private:
+  int year;
+  int month;
+  int day;
 };
 
-bool operator<(const Date& lhs, const Date& rhs);
+bool operator<(const Date& left, const Date& right) {
+  if (left.GetYear() < right.GetYear()) {
+    return true;
+  } else if (left.GetMonth() < right.GetMonth()) {
+    return true;
+  } else if (left.GetDay() <= right.GetDay()) {
+    return true;
+  } else
+    return false;
+};
+
+bool operator==(const Date& left, const Date& right) {
+  if ((left.GetYear() == right.GetYear()) && (left.GetMonth() == right.GetMonth()) && (left.GetDay() == right.GetDay())) {
+    return true;
+  } else
+    return false;
+}
 
 class Database {
 public:
   void AddEvent(const Date& date, const string& event);
   bool DeleteEvent(const Date& date, const string& event);
-  int  DeleteDate(const Date& date);
+  int DeleteDate(const Date& date);
 
   /* ??? */ Find(const Date& date) const;
-  
+
   void Print() const;
+
+private:
+  map<Date, string> base;
 };
 
 int main() {
   Database db;
-    
+
   string command;
   while (getline(cin, command)) {
     // Считайте команды с потока ввода и обработайте каждую
