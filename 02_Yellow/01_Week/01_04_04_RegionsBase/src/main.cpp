c7d8ed6f8a87c27d2064e2638397ace25382cd80
pavelzep <pavelzep@gmail.com>
2023-02-09 15:40:43 +0500

    01_04_04_RegionsBase finished

--- 02_Yellow/01_Week/01_04_04_RegionsBase/src/main.cpp
+++ 02_Yellow/01_Week/01_04_04_RegionsBase/src/main.cpp
@@ -17,6 +17,48 @@ struct Region {
 };
 
 int FindMaxRepetitionCount(const vector<Region>& regions) {
+   // if (regions.size() < 2) return regions.size();
+
+    map <tuple<const string&, const string&, const map<Lang, string>&, const int64_t&>, int> unics;
+    int rez = 0;
+
+    for (const auto& item : regions) {
+        auto t = tie(item.std_name, item.parent_std_name, item.names, item.population);
+        unics[t] = (unics.count(t) ? unics[t] + 1 : 1);
+        rez = unics[t]>rez? unics[t] : rez;
+    }
+    return rez;
+
+};
+
+int FindMaxRepetitionCount3(const vector<Region>& regions) {
+    auto temp = regions;
+    int rez = 0;
+    int base_size = temp.size();
+    if (base_size > 1) {
+        int count = 1;
+        for (int i = 0; i < base_size; i++) {
+
+            auto t1 = tie(temp.at(i).std_name, temp.at(i).parent_std_name, temp.at(i).names, temp.at(i).population);
+            count = 1;
+            for (int j = i + 1; j < base_size;j++) {
+                auto t2 = tie(temp.at(j).std_name, temp.at(j).parent_std_name, temp.at(j).names, temp.at(j).population);
+                if (t1 == t2) {
+                    count++;
+                    temp.erase(temp.begin() + j);
+                    base_size--;
+                    j--;
+                }
+                if (count > rez) rez = count;
+            }
+        }
+    } else {
+        return base_size;
+    }
+    return rez;
+};
+
+int FindMaxRepetitionCount2(const vector<Region>& regions) {
     int rez = 0;
     int base_size = regions.size();
     if (base_size > 1) {
@@ -50,6 +92,7 @@ int main() {
         {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
         89
     } }) << endl;
+
     cout << FindMaxRepetitionCount({
      {
          "Moscow",
