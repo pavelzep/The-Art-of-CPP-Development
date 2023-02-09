6cadcc9f594d6aed453706f92bd1da5c8f7aec77
pavelzep <pavelzep@gmail.com>
2023-02-09 13:06:20 +0500

    01_03_09_Blocks_Mass finished

--- 02_Yellow/01_Week/01_03_09_Blocks_Mass/src/main.cpp
+++ 02_Yellow/01_Week/01_03_09_Blocks_Mass/src/main.cpp
@@ -2,9 +2,30 @@
 
 
 using namespace std;
-int main(){
+int main() {
+    int N;          //  <100'000
+    int R;           //  <100
+    int W, H, D;    //  <10'000
+
+    cin >> N >> R;
+
+    uint64_t rez = 0 ;
 
 
-    
+    while (N)
+
+    {
+        cin >> W >> H >> D;
+        rez += (
+            static_cast<uint64_t>(W) *
+            static_cast<uint64_t>(H) *
+            static_cast<uint64_t>(D)
+            );
+        N--;
+    }
+
+    cout << rez*R;
+
+
     return 0;
 }
