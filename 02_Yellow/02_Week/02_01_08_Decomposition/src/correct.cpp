d26943f2e3ed23213db7d7a587e290c86bfbddeb
pavelzep <pavelzep@gmail.com>
2023-02-16 23:15:55 +0500

    v1

--- 02_Yellow/02_Week/02_01_08_Decomposition/src/correct.cpp
+++ 02_Yellow/02_Week/02_01_08_Decomposition/src/correct.cpp
@@ -6,32 +6,32 @@
 using namespace std;
 
 int main() {
-  int q;
-  cin >> q;
+//  int q;
+//  cin >> q;
 
-  map<string, vector<string>> buses_to_stops, stops_to_buses;
+//  map<string, vector<string>> buses_to_stops, stops_to_buses;
 
   for (int i = 0; i < q; ++i) {
-    string operation_code;
-    cin >> operation_code;
+//    string operation_code;
+//    cin >> operation_code;
 
     if (operation_code == "NEW_BUS") {
 
-      string bus;
-      cin >> bus;
-      int stop_count;
-      cin >> stop_count;
-      vector<string>& stops = buses_to_stops[bus];
-      stops.resize(stop_count);
-      for (string& stop : stops) {
-        cin >> stop;
-        stops_to_buses[stop].push_back(bus);
+//      string bus;
+//      cin >> bus;
+//      int stop_count;
+//      cin >> stop_count;
+//      vector<string>& stops = buses_to_stops[bus];
+//      stops.resize(stop_count);
+//      for (string& stop : stops) {
+//        cin >> stop;
+//        stops_to_buses[stop].push_back(bus);
       }
 
     } else if (operation_code == "BUSES_FOR_STOP") {
 
-      string stop;
-      cin >> stop;
+//      string stop;
+//      cin >> stop;
       if (stops_to_buses.count(stop) == 0) {
         cout << "No stop" << endl;
       } else {
@@ -43,8 +43,9 @@ int main() {
 
     } else if (operation_code == "STOPS_FOR_BUS") {
 
-      string bus;
-      cin >> bus;
+//      string bus;
+//      cin >> bus;
+
       if (buses_to_stops.count(bus) == 0) {
         cout << "No bus" << endl;
       } else {
--- 02_Yellow/02_Week/02_01_08_Decomposition/src/starter.cpp
+++ 02_Yellow/02_Week/02_01_08_Decomposition/src/starter.cpp
@@ -5,98 +5,130 @@
 #include <map>
 
 using namespace std;
+using store = map<string, vector<string>>;
 
 enum class QueryType {
-  NewBus,
-  BusesForStop,
-  StopsForBus,
-  AllBuses
+    NewBus,
+    BusesForStop,
+    StopsForBus,
+    AllBuses
 };
 
 struct Query {
-  QueryType type;
-  string bus;
-  string stop;
-  vector<string> stops;
+    QueryType type;
+    string bus;
+    string stop;
+    vector<string> stops;
 };
 
 istream& operator >> (istream& is, Query& q) {
-  // Реализуйте эту функцию
-  return is;
+    // Реализуйте эту функцию
+
+    string operation_code;
+    is >> operation_code;
+
+    if (operation_code == "NEW_BUS") {
+        q.type = QueryType::NewBus;
+        is >> q.bus;
+        int stop_count;
+        is >> stop_count;
+        q.stops.resize(stop_count);
+        for (string& stop : q.stops) {
+            is >> stop;
+            q.stops.push_back(q.bus);
+        }
+    } else if (operation_code == "BUSES_FOR_STOP") {
+        q.type = QueryType::BusesForStop;
+        is >> q.stop;
+
+    } else if (operation_code == "STOPS_FOR_BUS") {
+        q.type = QueryType::StopsForBus;
+        is >> q.bus;
+    } else if (operation_code == "ALL_BUSES") {
+        q.type = QueryType::AllBuses;
+    }
+
+
+
+    return is;
 }
 
 struct BusesForStopResponse {
-  // Наполните полями эту структуру
+    // Наполните полями эту структуру
 };
 
 ostream& operator << (ostream& os, const BusesForStopResponse& r) {
-  // Реализуйте эту функцию
-  return os;
+    // Реализуйте эту функцию
+    return os;
 }
 
 struct StopsForBusResponse {
-  // Наполните полями эту структуру
+    // Наполните полями эту структуру
 };
 
 ostream& operator << (ostream& os, const StopsForBusResponse& r) {
-  // Реализуйте эту функцию
-  return os;
+    // Реализуйте эту функцию
+    return os;
 }
 
 struct AllBusesResponse {
-  // Наполните полями эту структуру
+    // Наполните полями эту структуру
 };
 
 ostream& operator << (ostream& os, const AllBusesResponse& r) {
-  // Реализуйте эту функцию
-  return os;
+    // Реализуйте эту функцию
+    return os;
 }
 
 class BusManager {
 public:
-  void AddBus(const string& bus, const vector<string>& stops) {
-    // Реализуйте этот метод
-  }
+    void AddBus(const string& bus, const vector<string>& stops) {
+        // Реализуйте этот метод
+    }
 
-  BusesForStopResponse GetBusesForStop(const string& stop) const {
-    // Реализуйте этот метод
-  }
+    BusesForStopResponse GetBusesForStop(const string& stop) const {
+        // Реализуйте этот метод
+    }
 
-  StopsForBusResponse GetStopsForBus(const string& bus) const {
-    // Реализуйте этот метод
-  }
+    StopsForBusResponse GetStopsForBus(const string& bus) const {
+        // Реализуйте этот метод
+    }
 
-  AllBusesResponse GetAllBuses() const {
-    // Реализуйте этот метод
-  }
+    AllBusesResponse GetAllBuses() const {
+        // Реализуйте этот метод
+    }
+
+private:
+    store buses_to_stops, stops_to_buses;
+
 };
 
 // Не меняя тела функции main, реализуйте функции и классы выше
 
 int main() {
-  int query_count;
-  Query q;
+    int query_count;
+    Query q;
 
-  cin >> query_count;
+    cin >> query_count;
 
-  BusManager bm;
-  for (int i = 0; i < query_count; ++i) {
-    cin >> q;
-    switch (q.type) {
-    case QueryType::NewBus:
-      bm.AddBus(q.bus, q.stops);
-      break;
-    case QueryType::BusesForStop:
-      cout << bm.GetBusesForStop(q.stop) << endl;
-      break;
-    case QueryType::StopsForBus:
-      cout << bm.GetStopsForBus(q.bus) << endl;
-      break;
-    case QueryType::AllBuses:
-      cout << bm.GetAllBuses() << endl;
-      break;
+    BusManager bm;
+    for (int i = 0; i < query_count; ++i) {
+        cin >> q;
+        switch (q.type) {
+        case QueryType::NewBus:
+            bm.AddBus(q.bus, q.stops);
+            break;
+        case QueryType::BusesForStop:
+            cout << bm.GetBusesForStop(q.stop) << endl;
+            break;
+        case QueryType::StopsForBus:
+            cout << bm.GetStopsForBus(q.bus) << endl;
+            break;
+        case QueryType::AllBuses:
+            cout << bm.GetAllBuses() << endl;
+            break;
+        }
     }
-  }
 
-  return 0;
+    return 0;
 }
