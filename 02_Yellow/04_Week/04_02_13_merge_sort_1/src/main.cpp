3197b63b5dd0896366cd79201c9a2f62a66b4812
pavelzep <pavelzep@gmail.com>
2023-03-06 15:34:07 +0500

    v2

--- 02_Yellow/04_Week/04_02_13_merge_sort_1/src/main.cpp
+++ 02_Yellow/04_Week/04_02_13_merge_sort_1/src/main.cpp
@@ -59,11 +59,14 @@ void MergeSort(RandomIt range_begin, RandomIt range_end) {
     if ((range_end - range_begin) < 2) return;
     vector<typename RandomIt::value_type> elements(range_begin, range_end);
     auto it = elements.begin() + (elements.end() - elements.begin()) / 2;
-    vector<typename RandomIt::value_type> v1(elements.begin(), it);
-    vector<typename RandomIt::value_type> v2(it, elements.end());
-    MergeSort(v1.begin(), v1.end());
-    MergeSort(v2.begin(), v2.end());
-    merge(v1.begin(), v1.end(), v2.begin(), v2.end(), range_begin);
+    // vector<typename RandomIt::value_type> v1(elements.begin(), it);
+    // vector<typename RandomIt::value_type> v2(it, elements.end());
+    // MergeSort(v1.begin(), v1.end());
+    // MergeSort(v2.begin(), v2.end());
+    MergeSort(elements.begin(), it);
+    MergeSort(it, elements.end());
+
+    merge(elements.begin(), it, it, elements.end(), range_begin);
 }
 
 
@@ -77,3 +80,5 @@ int main() {
     cout << endl;
     return 0;
 }
+
+
