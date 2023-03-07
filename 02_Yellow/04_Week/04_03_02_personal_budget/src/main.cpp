33c370b2aa596fcc7a0866ba3a4d2c2672792b94
pavelzep <pavelzep@gmail.com>
2023-03-07 13:32:28 +0500

    v1 accepted

--- 02_Yellow/04_Week/04_03_02_personal_budget/src/main.cpp
+++ 02_Yellow/04_Week/04_03_02_personal_budget/src/main.cpp
@@ -28,39 +28,45 @@
 // 20
 // 8
 // 30
+
 #include <iostream>
 #include <algorithm>
 #include <numeric>
 #include <string>
+#include <ctime>
+#include <iomanip>
+#include <vector>
 
 using namespace std;
-
 
 enum class QueryType {
     ComputeIncome,
     Earn
 };
 
-struct Date {
-    int year;
-    int month;
-    int day;
-};
+tm start = { 0,0,0,1,0,100 };
+tm fin = { 0,0,0,31,11,199 };
 
 struct Query {
     QueryType type;
-    Date from;
-    Date to;
+    tm from = {};
+    tm to = {};
+    double money;
 };
 
-istream& operator >> (istream& is, Date& d){
 
-    is >> d.year;
-    //пропуск символа.
-    is >> d.month;
-   //пропуск символа.
-    is >> d.day;
+istream& operator >> (istream& is, tm& from) {
+    int temp;
+    is >> temp;
 
+    from.tm_year = temp - 1900;
+    is.ignore(1);
+    is >> temp;
+    from.tm_mon = temp - 1;
+    is.ignore(1);
+    is >> temp;
+    from.tm_mday = temp;
+    return is;
 }
 
 istream& operator >> (istream& is, Query& q) {
@@ -69,30 +75,63 @@ istream& operator >> (istream& is, Query& q) {
 
     if (operation_code == "ComputeIncome") {
         q.type = QueryType::ComputeIncome;
-
-    } else if ((operation_code == "Earn") {
+    } else if (operation_code == "Earn") {
         q.type = QueryType::Earn;
     }
 
-    
+    is >> q.from;
 
+    is >> q.to;
+    if (q.type == QueryType::Earn) {
+        is >> q.money;
+    }
     return is;
+}
+
+int position_from_date(tm& date) {
+
+    return difftime(mktime(&date), mktime(&start)) / (60 * 60 * 24);
+
+}
+
+void Response(Query& q, vector<double>& in) {
+
+    auto start_it = in.begin() + position_from_date(q.from);
+    auto end_it = in.begin() + position_from_date(q.to);
+
+    switch (q.type) {
+    case QueryType::Earn: {
+
+        // сколько дней между датами включая крайние даты
+        int days = difftime(mktime(&q.to), mktime(&q.from)) / (60 * 60 * 24) + 1;
+        // доход в день
+        double earn_for_day = q.money / days;
+        for (auto it = start_it; it != next(end_it); ++it) {
+            *it += earn_for_day;
+        }
+        break;
+    }
+    case QueryType::ComputeIncome: {
+        cout << setprecision(25) << accumulate(start_it, end_it + 1, 0.) << endl;
+
+        break;
+    }
+    }
 }
 
 int main() {
 
-    int q;
-        cin >> q;
+    vector<double> income(difftime(mktime(&fin), mktime(&start)) / (60 * 60 * 24) + 1);
 
+    int query_count;
+    cin >> query_count;
 
-    while (q) {
-        --q;
+    Query query;
 
-
-
-
-
-
+    while (query_count) {
+        --query_count;
+        cin >> query;
+        Response(query, income);
     }
     return 0;
 }
