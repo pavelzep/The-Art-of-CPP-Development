d3d9b6fcc69695128c058c37d4646fd8c4b916d9
pavelzep <pavelzep@gmail.com>
2023-03-07 16:44:42 +0500

    v2

--- 02_Yellow/04_Week/04_03_03_personal_budget_2/src/main.cpp
+++ 02_Yellow/04_Week/04_03_03_personal_budget_2/src/main.cpp
@@ -31,7 +31,74 @@
 // 20
 // 40
 
-int main() {
-    return 0;
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <ctime>
+#include <vector>
+
+
+using namespace std;
+
+tm string_to_date(const string& date_) {
+    stringstream ss(date_);
+    tm date = {};
+    ss >> date.tm_year;
+    date.tm_year -= 1900;
+    ss.ignore(1);
+    ss >> date.tm_mon;
+    date.tm_mon -= 1;
+    ss.ignore(1);
+    ss >> date.tm_mday;
+    return date;
 }
 
+tm MIN_DATE = { 0,0,0,1,0,100 };
+tm MAX_DATE = { 0,0,0,1,1,200 };
+
+int position_from_date(tm& date) {
+    return difftime(mktime(&date), mktime(&MIN_DATE)) / (60 * 60 * 24);
+}
+
+void Processing_request(const string& date_, const int value_, vector<double>& store_) {
+    tm date = string_to_date(date_);
+    int pos = position_from_date(date);
+    store_[pos] += value_;
+}
+
+void Processing_request(const string& from_, const string& to_, vector<double>& store) {
+    tm tm_from = string_to_date(from_);
+    int pos_from = position_from_date(tm_from);
+
+    tm tm_to = string_to_date(to_);
+    int pos_to = position_from_date(tm_to);
+
+    
+
+
+}
+
+
+int main() {
+    vector<double> store(position_from_date(MAX_DATE));
+
+    int e_count, q_count;
+    cin >> e_count;
+
+    while (e_count) {
+        string date;
+        int value;
+        cin >> date >> value;
+        Processing_request(date, value, store);
+        --e_count;
+    }
+
+    cin >> q_count;
+    while (q_count) {
+        string from, to;
+        cin >> from >> to;
+        Processing_request(from, to, store);
+        --q_count;
+    }
+    return 0;
+}
