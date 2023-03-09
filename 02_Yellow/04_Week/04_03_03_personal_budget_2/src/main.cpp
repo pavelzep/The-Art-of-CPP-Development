99cf034d99ca38a0f0d86a735228e9d4b9adb844
pavelzep <pavelzep@gmail.com>
2023-03-09 15:23:46 +0500

    v6

--- 02_Yellow/04_Week/04_03_03_personal_budget_2/src/main.cpp
+++ 02_Yellow/04_Week/04_03_03_personal_budget_2/src/main.cpp
@@ -5,21 +5,21 @@
 // Cначала идут только запросы с информацией о заработках, а потом — только запросы на вычисление прибыли.
 // Примечания:
 // Во всех диапазонах from to обе даты from и to включаются.
-// Формат ввода
+// Формат ввода:
 // В первой строке вводится количество дней с информацией о заработках E, затем в описанном выше формате вводятся запросы, 
 // по одному на строке. Затем вводится количество запросов Q на вычисление прибыли, а после в описанном выше формате вводятся
 // запросы на вычисление прибыли, по одному на строке.
-// Формат вывода
+// Формат вывода:
 // Для каждого запроса на вычисление прибыли в отдельной строке выведите целое число — прибыль за указанный диапазон дат.
-// Ограничения
+// Ограничения:
 // Количество запросов Q — натуральное число, не превышающее 100000.
 // Все даты вводятся в формате YYYY-MM-DD. Даты корректны (с учётом високосных годов) и принадлежат интервалу с 1700 до 2099 гг.
 // value — положительные целые числа, не превышающие 1000000.
 // 4.9 секунды на обработку всех запросов.
-// Подсказка
+// Подсказка:
 // Используйте std::partial_sum, чтобы отвечать на каждый запрос, считая разность между двумя частичными суммами.
-// Пример
-// Ввод
+// Пример:
+// Ввод:
 // 3
 // 2000-01-02 20
 // 2000-01-06 10
@@ -27,7 +27,7 @@
 // 2
 // 2000-01-01 2000-01-02
 // 2000-01-02 2000-01-06
-// Вывод
+// Вывод:
 // 20
 // 40
 
@@ -41,26 +41,33 @@
 
 using namespace std;
 
-int position_from_date(const string& date_, vector<int>& sum_of_days_);
-
-void Processing_request_1(const string& date_, const int value_, vector<uint32_t>& store_, vector<int>& sum_of_days_);
-void Processing_request_2(const string& from_, const string& to_, vector<uint32_t>& store, vector<int>& sum_of_days_);
-
-void init();
-
-void init(vector<int>& days_from_month_, vector<int>& sum_of_days_) {
-
-    partial_sum(days_from_month_.begin(), prev(days_from_month_.end()), back_inserter(sum_of_days_));
-}
-
 struct Date {
     int year;
     int month;
     int day;
-
 };
 
-int position_from_date(const string& date_, vector<int>& sum_of_days_) {
+const string min_date_str = "1900-01-01";
+const string max_date_str = "2099-12-31";
+
+vector<int> days_from_month = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+vector<int> sum_of_days = { 0 };
+
+Date date_from_string(const string& date_);
+
+Date min_date = date_from_string(min_date_str);
+Date max_date = date_from_string(max_date_str);
+
+int position_from_date(const Date& date_);
+
+void Processing_request_1(const string& date_, const int value_, vector<uint32_t>& store_);
+
+void Processing_request_2(const string& from_, const string& to_, vector<uint32_t>& store_);
+
+void init();
+
+Date date_from_string(const string& date_) {
+
     stringstream ss(date_);
     Date date;
     ss >> date.year;
@@ -68,60 +75,68 @@ int position_from_date(const string& date_, vector<int>& sum_of_days_) {
     ss >> date.month;
     ss.ignore(1);
     ss >> date.day;
-
-    int q = 0;
-    for (int i = 1901; i < date.year;++i) {
-        if ((i % 4 == 0)) {
-            ++q;
-        }
-    }
-    int result = (date.year - 1900) * 365 + sum_of_days_[date.month - 1] + date.day + q;
-    return result;
+    return date;
 }
 
-void Processing_request_1(const string& date_, const int value_, vector<uint32_t>& store_, vector<int>& sum_of_days_) {
+int position_from_date(const Date& date_) {
 
-    int pos = position_from_date(date_, sum_of_days_);
+    int leap_year_correction = 0;
+    for (int i = min_date.year; i <= date_.year;++i) {
+        if ((i % 4 == 0) && (i != 1700) && (i != 1800) && (i != 1900)) {
+            ++leap_year_correction;
+        }
+    }
+
+    int position = (date_.year - min_date.year) * 365 + sum_of_days[date_.month - 1] + date_.day + leap_year_correction - 1;
+    return position;
+}
+
+void Processing_request_1(const string& date_, const int value_, vector<uint32_t>& store_) {
+    int pos = position_from_date(date_from_string(date_));
     store_[pos] += value_;
 }
 
-void Processing_request_2(const string& from_, const string& to_, vector<uint32_t>& store, vector<int>& sum_of_days_) {
-    int result;
+void Processing_request_2(const string& from_, const string& to_, vector<uint32_t>& store_) {
 
-    int pos_from = position_from_date(from_, sum_of_days_);
-    int pos_to = position_from_date(to_, sum_of_days_);
+    int pos_from = position_from_date(date_from_string(from_));
+    int pos_to = position_from_date(date_from_string(to_));
 
     vector<int> sum_vect1;
     vector<int> sum_vect2;
 
-    auto it_start = store.begin() + pos_from;
-    auto it_fin = store.begin() + pos_to;
+    auto it_start = store_.begin() + pos_from;
+    auto it_fin = store_.begin() + pos_to;
 
-    partial_sum(store.begin(), it_start + 1, back_inserter(sum_vect1));
-    partial_sum(store.begin(), it_fin + 1, back_inserter(sum_vect2));
+    partial_sum(store_.begin(), it_start + 1, back_inserter(sum_vect1));
+    partial_sum(store_.begin(), it_fin + 1, back_inserter(sum_vect2));
 
-    result = sum_vect2.back() - sum_vect1.back() + store[pos_from];
+    int result = sum_vect2.back() - sum_vect1.back() + store_[pos_from];
     cout << result << endl;
+}
+
+void init() {
+    partial_sum(days_from_month.begin(), prev(days_from_month.end()), back_inserter(sum_of_days));
 }
 
 int main() {
 
-    string min_date_str = "1900-01-01";
-    string max_date_str = "2099-12-31";
+    init();
 
-    vector<int> days_from_month{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    vector<int> sum_of_days = { 0 };
+    vector<uint32_t> store(position_from_date(date_from_string(max_date_str)) + 1);
 
-    init(days_from_month, sum_of_days);
 
-    vector<uint32_t> store(position_from_date(max_date_str, sum_of_days));
-    int size_v = store.size();
+    // Debug:
 
-    int temp = position_from_date("1900-01-31", sum_of_days);
+    // int size = store.size();
+    // int debug_ = 0;
+
+    // debug_ = position_from_date(date_from_string("1900-01-01"));
+    // debug_ = position_from_date(date_from_string("2000-01-01"));
+    // debug_ = position_from_date(date_from_string("2099-12-31"));
+    
+    // fstream cin("../input.txt");
 
     int e_count, q_count;
-
-    fstream cin("../input.txt");
 
     cin >> e_count;
 
@@ -129,7 +144,7 @@ int main() {
         string date;
         int value;
         cin >> date >> value;
-        Processing_request_1(date, value, store, sum_of_days);
+        Processing_request_1(date, value, store);
         --e_count;
     }
 
@@ -137,7 +152,7 @@ int main() {
     while (q_count) {
         string from, to;
         cin >> from >> to;
-        Processing_request_2(from, to, store, sum_of_days);
+        Processing_request_2(from, to, store);
         --q_count;
     }
     return 0;
