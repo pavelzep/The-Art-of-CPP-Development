c63d2a559f5b2218ffd6047c8c8e7d7ec337d984
pavelzep <pavelzep@gmail.com>
2023-04-03 16:13:36 +0500

    v0

--- 02_Yellow/05_Week/05_02_10_Notifier/src/main.cpp
+++ 02_Yellow/05_Week/05_02_10_Notifier/src/main.cpp
@@ -51,25 +51,39 @@
 using namespace std;
 
 void SendSms(const string& number, const string& message) {
-  cout << "Send '" << message << "' to number " << number << endl;
+    cout << "Send '" << message << "' to number " << number << endl;
 }
 
 void SendEmail(const string& email, const string& message) {
-  cout << "Send '" << message << "' to e-mail "  << email << endl;
+    cout << "Send '" << message << "' to e-mail " << email << endl;
 }
 
 /*
  Реализуйте здесь классы INotifier, SmsNotifier, EmailNotifier
  */
 
+class INotifier {
+
+}
+
+class SmsNotifier: public INotifier {
+
+}
+
+class EmailNotifier: 
+
+
+
+
+
 void Notify(INotifier& notifier, const string& message) {
-  notifier.Notify(message);
+    notifier.Notify(message);
 }
 
 int main() {
-  SmsNotifier sms{"+7-495-777-77-77"};
-  EmailNotifier email{"na-derevnyu@dedushke.ru"};
-  Notify(sms, "I have White belt in C++");
-  Notify(email, "And want a Yellow one");
-  return 0;
+    SmsNotifier sms{ "+7-495-777-77-77" };
+    EmailNotifier email{ "na-derevnyu@dedushke.ru" };
+    Notify(sms, "I have White belt in C++");
+    Notify(email, "And want a Yellow one");
+    return 0;
 }
