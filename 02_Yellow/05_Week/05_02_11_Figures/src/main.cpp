9bb0e13adaeb049664ed3011c644560d2ca1dfa7
pavelzep <pavelzep@gmail.com>
2023-04-04 16:48:15 +0500

    v2

--- 02_Yellow/05_Week/05_02_11_Figures/src/main.cpp
+++ 02_Yellow/05_Week/05_02_11_Figures/src/main.cpp
@@ -1,90 +1,120 @@
 
 #include <iostream>
 #include <sstream>
+#include <fstream>
 #include <vector>
 #include <memory>
 #include <iomanip>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
 class Figure {
 public:
-    Figure(string& name): name_(name) {
+    Figure(string& name, vector<int>& params): name_(name), params_(params) {
 
     };
+
     virtual string Name() const = 0;
-    virtual int Perimeter() const = 0;
-    virtual int Area() const = 0;
+    virtual double Perimeter() const = 0;
+    virtual double Area() const = 0;
 protected:
     string name_;
-
-
+    vector<int> params_;
 };
 
 class Triangle: public Figure {
 public:
-    Triangle(string& name, int a, int b, int c): Figure(name), a_(a), b_(b), c_(c) {
+    Triangle(string& name, vector<int>& params): Figure(name, params) {
 
     };
 
     string Name() const override {
-
+        return name_;
     };
-    int Perimeter()const override {
-
+    double Perimeter()const override {
+        return  params_.at(0) + params_.at(1) + params_.at(2);
     };
-    int Area() const override {
-
+    double Area() const override {
+        double p = (params_.at(0) + params_.at(1) + params_.at(2)) / 2;
+        return sqrt(p * (p - params_.at(0)) * (p - params_.at(1)) * (p - params_.at(2)));
     };
 private:
-    int a_;
-    int b_;
-    int c_;
+
 };
 
 class Rect: public Figure {
 public:
-    Rect(string& name, int a, int b): Figure(name), a_(a), b_(b) {
+    Rect(string& name, vector<int>& params): Figure(name, params) {
 
     };
     string Name() const override {
-
+        return name_;
     };
-    int Perimeter() const override {
 
+    double Perimeter() const override {
+        return (params_.at(0) + params_.at(1)) * 2;
     };
-    int Area() const override {
 
+    double Area() const override {
+        return params_.at(0) * params_.at(1);
     };
 private:
-    int a_;
-    int b_;
+
 };
 
 class Circle: public Figure {
 public:
-    Circle(string& name, int a): Figure(name), a_(a) {
+    Circle(string& name, vector<int>& params): Figure(name, params) {
 
     };
     string Name() const override {
-
+        return name_;
     };
-    int Perimeter() const override {
-
+    double Perimeter() const override {
+        return params_.at(0) * 2 * 3.14;
     };
-    int Area() const  override {
-
+    double Area() const  override {
+        return params_.at(0) * params_.at(0) * 3.14;
     };
 private:
     int a_;
 };
 
-shared_ptr<Figure> CreateFigure(istringstream& _is) {
-    return {};
+
+
+
+istringstream& operator >> (istringstream& is, vector<int>& vect) {
+    int t;
+    while (is >> t) {
+        vect.push_back(t);
+    }
+    return is;
+}
+
+shared_ptr<Figure> CreateFigure(istringstream& is) {
+    string name;
+    is >> name;
+    vector<int> params;
+    is >> params;
+    shared_ptr<Figure> figure_;
+    if (name == "RECT") {
+        figure_ = make_shared<Rect>(name, params);
+    } else if (name == "TRIANGLE") {
+        figure_ = make_shared<Triangle>(name, params);
+    } else  if (name == "CIRCLE") {
+        figure_ = make_shared<Circle>(name, params);
+    }
+
+    return figure_;
 }
 
 int main() {
+
+
+    fstream cin("../input.txt");
+
     vector<shared_ptr<Figure>> figures;
     for (string line; getline(cin, line); ) {
         istringstream is(line);
