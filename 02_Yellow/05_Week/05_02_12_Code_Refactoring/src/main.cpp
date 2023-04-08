89c853b9183b7e5c4e2986e66ee22fbd75a70962
pavezep <pavelzep@gmail.com>
2023-04-08 15:19:24 +0500

    fin

--- 02_Yellow/05_Week/05_02_12_Code_Refactoring/src/main.cpp
+++ 02_Yellow/05_Week/05_02_12_Code_Refactoring/src/main.cpp
@@ -6,31 +6,30 @@ using namespace std;
 
 class Person {
 public:
-    Person(const string& name):Name(name) {
-
+    Person(const string& name, const string& type):Name(name), Type(type) {}
+    virtual void Walk(const string& destination) const {
+        cout << Type << ": " << Name << " walks to: " << destination << endl;
     }
-    virtual void Walk(const string& destination) = 0;
     string Name;
+    string Type;
 };
 
 class Student: public Person {
 public:
-
-    Student(const string& name, const string& favouriteSong): Person(name) {
-        FavouriteSong = favouriteSong;
+    Student(const string& name, const string& favouriteSong): Person(name, "Student"), FavouriteSong(favouriteSong) {
     }
 
-    void Walk(const string& destination) override {
-        cout << "Student: " << Name << " walks to: " << destination << endl;
-        cout << "Student: " << Name << " sings a song: " << FavouriteSong << endl;
+    void Walk(const string& destination) const override {
+        Person::Walk(destination);
+        SingSong();
     }
 
-    void Learn() {
-        cout << "Student: " << Name << " learns" << endl;
+    void Learn() const {
+        cout << Type << ": " << Name << " learns" << endl;
     }
 
-    void SingSong() {
-        cout << "Student: " << Name << " sings a song: " << FavouriteSong << endl;
+    void SingSong() const {
+        cout << Type << ": " << Name << " sings a song: " << FavouriteSong << endl;
     }
 
 private:
@@ -40,18 +39,10 @@ private:
 
 class Teacher: public Person {
 public:
+    Teacher(const string& name, const string& subject): Person(name, "Teacher"), Subject(subject) {}
 
-    Teacher(const string& name, const string& subject):Person(name) {
-
-        Subject = subject;
-    }
-
-    void Walk(const string& destination) override {
-        cout << "Teacher: " << Name << " walks to: " << destination << endl;
-    }
-
-    void Teach() {
-        cout << "Teacher: " << Name << " teaches: " << Subject << endl;
+    void Teach() const {
+        cout << Type << ": " << Name << " teaches: " << Subject << endl;
     }
 
 private:
@@ -61,45 +52,19 @@ private:
 
 class Policeman: public Person {
 public:
-    Policeman(const string& name):Person(name) {
+    Policeman(const string& name): Person(name, "Policeman") {}
+
+    void Check(const Person& t) const {
+        cout << Type << ": " << Name << " checks " << t.Type << ". " << t.Type << "'s name is: " << t.Name << endl;
     }
 
-    void Check(Teacher& t) {
-        cout << "Policeman: " << Name << " checks Teacher. Teacher's name is: " << t.Name << endl;
-    }
-
-    void Check(Student& s) {
-        cout << "Policeman: " << Name << " checks Student. Student's name is: " << s.Name << endl;
-    }
-
-    void Check(Policeman& p) {
-        cout << "Policeman: " << Name << " checks Policeman. Policeman's name is: " << p.Name << endl;
-    }
-
-    void Walk(const string& destination) override {
-        cout << "Policeman: " << Name << " walks to: " << destination << endl;
-    }
 };
 
-
-void VisitPlaces(Teacher& t, const vector<string>& places) {
-    for (auto p : places) {
-        t.Walk(p);
+void VisitPlaces(const Person& p, const vector<string>& places) {
+    for (auto pl : places) {
+        p.Walk(pl);
     }
 }
-
-void VisitPlaces(Student& s, const vector<string>& places) {
-    for (auto p : places) {
-        s.Walk(p);
-    }
-}
-
-void VisitPlaces(Policeman& pol, const vector<string>& places) {
-    for (auto p : places) {
-        pol.Walk(p);
-    }
-}
-
 
 int main() {
     Teacher t("Jim", "Math");
