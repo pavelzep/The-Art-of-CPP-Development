1dc4c1b54fcd53835bc4996640eda68ceeeb61be
pavelzep <pavelzep@gmail.com>
2023-04-28 16:31:55 +0500

    v0

---
+++ 03_Red/01_Week/01_04_13_macro_UNIQ_ID/src/main.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+#define UNIQ_ID (__FILE__)##(__LINE__)
+int main() {
+  int UNIQ_ID = 0;
+  string UNIQ_ID = "hello";
+  vector<string> UNIQ_ID = {"hello", "world"};
+  vector<int> UNIQ_ID = {1, 2, 3, 4};
+}
