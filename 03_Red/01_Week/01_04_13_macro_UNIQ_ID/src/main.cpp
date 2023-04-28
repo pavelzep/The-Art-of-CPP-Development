0d16d4ca99c14ec47d97fd4d7a808cf15d3a83b0
pavelzep <pavelzep@gmail.com>
2023-04-28 23:17:07 +0500

    v1

--- 03_Red/01_Week/01_04_13_macro_UNIQ_ID/src/main.cpp
+++ 03_Red/01_Week/01_04_13_macro_UNIQ_ID/src/main.cpp
@@ -2,10 +2,14 @@
 #include <vector>
 using namespace std;
 
-#define UNIQ_ID (__FILE__)##(__LINE__)
+
+#define b(x) var##x 
+#define a(x) b(x)
+#define UNIQ_ID a(__LINE__)
+
 int main() {
-  int UNIQ_ID = 0;
-  string UNIQ_ID = "hello";
-  vector<string> UNIQ_ID = {"hello", "world"};
-  vector<int> UNIQ_ID = {1, 2, 3, 4};
+    int UNIQ_ID = 0;
+    string UNIQ_ID = "hello";
+    vector<string> UNIQ_ID = { "hello", "world" };
+    vector<int> UNIQ_ID = { 1, 2, 3, 4 };
 }
