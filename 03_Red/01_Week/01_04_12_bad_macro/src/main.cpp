2ba28242bb28225871789d814de2b9c7884a8d5d
pavelzep <pavelzep@gmail.com>
2023-04-28 15:41:53 +0500

    accepted

--- 03_Red/01_Week/01_04_12_bad_macro/src/main.cpp
+++ 03_Red/01_Week/01_04_12_bad_macro/src/main.cpp
@@ -1,15 +1,19 @@
 #include "test_runner.h"
 
-#include <ostream>
+#
+#include <iostream>
 using namespace std;
 
-#define PRINT_VALUES(out, x, y) out << (x) << endl; out << (y) << endl
+#define PRINT_VALUES(out, x, y) out << (x) << endl <<(y) << endl
+
 
 int main() {
-  TestRunner tr;
-  tr.RunTest([] {
-    ostringstream output;
-    PRINT_VALUES(output, 5, "red belt");
-    ASSERT_EQUAL(output.str(), "5\nred belt\n");
-  }, "PRINT_VALUES usage example");
+    TestRunner tr;
+    tr.RunTest([] {
+        ostringstream output;
+        PRINT_VALUES(output, 5, "red belt");
+        ASSERT_EQUAL(output.str(), "5\nred belt\n");
+        }, "PRINT_VALUES usage example");
+    return 0;
 }
+
