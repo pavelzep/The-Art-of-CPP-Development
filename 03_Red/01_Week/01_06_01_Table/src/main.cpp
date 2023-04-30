d494d464d04939f74b12177cbba0091d7ecd75e9
pavelzep <pavelzep@gmail.com>
2023-04-30 23:20:40 +0500

    v0

--- 03_Red/01_Week/01_06_01_Table/src/main.cpp
+++ 03_Red/01_Week/01_06_01_Table/src/main.cpp
@@ -2,24 +2,26 @@
 
 using namespace std;
 
+template<typename T>
+vector<vector<T>> Table(size_t x_, size_t y_) {
 
-
+};
 
 
 
 void TestTable() {
-  Table<int> t(1, 1);
-  ASSERT_EQUAL(t.Size().first, 1u);
-  ASSERT_EQUAL(t.Size().second, 1u);
-  t[0][0] = 42;
-  ASSERT_EQUAL(t[0][0], 42);
-  t.Resize(3, 4);
-  ASSERT_EQUAL(t.Size().first, 3u);
-  ASSERT_EQUAL(t.Size().second, 4u);
+    Table<int> t(1, 1);
+    ASSERT_EQUAL(t.Size().first, 1u);
+    ASSERT_EQUAL(t.Size().second, 1u);
+    t[0][0] = 42;
+    ASSERT_EQUAL(t[0][0], 42);
+    t.Resize(3, 4);
+    ASSERT_EQUAL(t.Size().first, 3u);
+    ASSERT_EQUAL(t.Size().second, 4u);
 }
 
 int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestTable);
-  return 0;
+    TestRunner tr;
+    RUN_TEST(tr, TestTable);
+    return 0;
 }
