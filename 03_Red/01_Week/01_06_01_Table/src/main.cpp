b4be8dc0dee67fd43e3ec769f198ab505015f4ed
pavelzep <pavelzep@gmail.com>
2023-05-01 11:43:00 +0500

    accepted

--- 03_Red/01_Week/01_06_01_Table/src/main.cpp
+++ 03_Red/01_Week/01_06_01_Table/src/main.cpp
@@ -3,8 +3,47 @@
 using namespace std;
 
 template<typename T>
-vector<vector<T>> Table(size_t x_, size_t y_) {
+class Table {
+public:
+    Table(const size_t row_, const size_t column_):row(0u), column(0u) {
+        store.resize(row_);
+        if (row_ && column_) {
+            for (auto& row : store) {
+                row.resize(column_);
+            }
+            row = row_;
+            column = column_;
+        }
+    };
 
+    void Resize(const size_t row_, const size_t column_) {
+        store.resize(row_);
+        if (row_ && column_) {
+            for (auto& row : store) {
+                row.resize(column_);
+            }
+            row = row_;
+            column = column_;
+        }
+    };
+
+    pair< size_t, size_t >Size() const {
+        return make_pair(row, column);
+    };
+
+    vector<T>& operator[](size_t index) {
+        return store[index];
+    };
+
+    const vector<T>& operator[](size_t index) const {
+        return store[index];
+    };
+
+
+
+private:
+    vector<vector<T>> store;
+    size_t row, column;
 };
 
 
@@ -20,8 +59,42 @@ void TestTable() {
     ASSERT_EQUAL(t.Size().second, 4u);
 }
 
+void MyTest() {
+    {
+        Table<int> t(5, 5);
+        t[4][4] = 42;
+        ASSERT_EQUAL(t[4][4], 42);
+        t.Resize(7, 7);
+        ASSERT_EQUAL(t[4][4], 42);
+        t[3][3] = 55;
+        t.Resize(5, 5);
+        ASSERT_EQUAL(t[4][4], 42);
+        t.Resize(4, 4);
+        ASSERT_EQUAL(t[3][3], 55);
+    }
+}
+void MyTest2() {
+    {
+        Table<int> t(0, 2);
+        ASSERT_EQUAL(t.Size().first, 0u);
+        ASSERT_EQUAL(t.Size().second, 0u);
+    }
+    {
+        Table<int> t(2, 0);
+        ASSERT_EQUAL(t.Size().first, 0u);
+        ASSERT_EQUAL(t.Size().second, 0u);
+    }
+    {
+        Table<int> t(0, 0);
+        ASSERT_EQUAL(t.Size().first, 0u);
+        ASSERT_EQUAL(t.Size().second, 0u);
+    }
+}
 int main() {
     TestRunner tr;
     RUN_TEST(tr, TestTable);
+    RUN_TEST(tr, MyTest);
+    RUN_TEST(tr, MyTest2);
+
     return 0;
 }
