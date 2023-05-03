7d2bae9ddf00427903e64b129d3d9807ab046396
pavelzep <pavelzep@gmail.com>
2023-05-03 11:53:07 +0500

    v1

---
+++ 03_Red/01_Week/01_06_02_2Vector_Deque/src/deque.h
@@ -0,0 +1,94 @@
+#include <vector>
+#include <exception>
+
+using namespace std;
+
+
+template<typename T>
+class Deque {
+public:
+    Deque() {
+
+    }
+
+    bool Empty() const {
+        return (front.empty() && back.empty());
+    }
+    size_t Size() const {
+        return front.size() + back.size();
+    }
+
+    T& At(size_t index) {
+        if (!(index < Size())) throw out_of_range("wrong index");
+        if (index >= front.size()) {
+            index -= front.size();
+            return back[index];
+        } else {
+            return front[front.size() - index - 1];
+        }
+    }
+
+    const T& At(size_t index) const {
+        if (!(index < Size())) throw out_of_range("wrong index");
+        if (index >= front.size()) {
+            index -= front.size();
+            return back[index];
+        } else {
+            return front[front.size() - index - 1];
+        }
+    }
+
+    T& operator[](size_t index) {
+        if (index >= front.size()) {
+            index -= front.size();
+            return back[index];
+        } else {
+            return front[front.size() - index - 1];
+        }
+    }
+
+    const T& operator[](size_t index) const {
+
+        if (index >= front.size()) {
+            index -= front.size();
+            return back[index];
+        } else {
+            return front[front.size() - index - 1];
+        }
+    }
+
+    T& Front() {
+        if (front.empty()) return back.front();
+        return front.back();
+    }
+
+    const T& Front() const {
+        if (front.emty()) return back.front();
+        return front.back();
+    }
+
+    T& Back() {
+        if (back.empty()) return front.front();
+        return back.back();
+    }
+
+    const T& Back() const {
+        if (back.empty()) return front.front();
+        return back.back();
+    }
+
+    void PushFront(const T& t) {
+        front.push_back(t);
+    }
+
+    void PushBack(const T& t) {
+        back.push_back(t);
+    }
+
+
+private:
+
+    vector<T> front;
+    vector<T> back;
+
+};
---
+++ 03_Red/01_Week/01_06_02_2Vector_Deque/src/my_test.h
@@ -0,0 +1,96 @@
+void test_1() {
+    Deque<int> t;
+    ASSERT_EQUAL(t.Size(), 0u);
+}
+void test_2() {
+    Deque<int> t;
+    t.PushBack(42);
+    ASSERT_EQUAL(t.Size(), 1u);
+    ASSERT_EQUAL(t.Back(), 42);
+}
+void test_3() {
+    Deque<int> t;
+    t.PushBack(42);
+    ASSERT_EQUAL(t.Size(), 1u);
+    ASSERT_EQUAL(t.Front(), 42);
+}
+void test_4() {
+    Deque<int> t;
+    t.PushBack(42);
+    t.PushBack(43);
+    t.PushBack(44);
+    t.PushBack(45);
+
+    ASSERT_EQUAL(t.Size(), 4u);
+    ASSERT_EQUAL(t[0], 42);
+    ASSERT_EQUAL(t[1], 43);
+    ASSERT_EQUAL(t[2], 44);
+    ASSERT_EQUAL(t[3], 45);
+}
+void test_5() {
+    Deque<int> t;
+    t.PushFront(42);
+    t.PushFront(43);
+    t.PushFront(44);
+    t.PushFront(45);
+
+    ASSERT_EQUAL(t.Size(), 4u);
+    ASSERT_EQUAL(t[3], 42);
+    ASSERT_EQUAL(t[2], 43);
+    ASSERT_EQUAL(t[1], 44);
+    ASSERT_EQUAL(t[0], 45);
+}
+void test_6() {
+    Deque<int> t;
+
+    t.PushFront(42);
+    t.PushFront(43);
+    t.PushFront(44);
+    t.PushFront(45);
+    t.PushBack(41);
+    t.PushBack(40);
+    t.PushBack(39);
+    t.PushBack(38);
+
+    ASSERT_EQUAL(t.Size(), 8u);
+    ASSERT_EQUAL(t[7], 38);
+    ASSERT_EQUAL(t[6], 39);
+    ASSERT_EQUAL(t[5], 40);
+    ASSERT_EQUAL(t[4], 41);
+    ASSERT_EQUAL(t[3], 42);
+    ASSERT_EQUAL(t[2], 43);
+    ASSERT_EQUAL(t[1], 44);
+    ASSERT_EQUAL(t[0], 45);
+
+    t[2] = 65;
+    t[3] = 66;
+    t[4] = 67;
+    t[5] = 68;
+
+    ASSERT_EQUAL(t[2], 65);
+    ASSERT_EQUAL(t[3], 66);
+    ASSERT_EQUAL(t[4], 67);
+    ASSERT_EQUAL(t[5], 68);
+}
+void test_7() {
+    Deque<int> t;
+    t.PushFront(9);
+    ASSERT_EQUAL(t[0], 9);
+    t.Front() = 8;
+    ASSERT_EQUAL(t[0], 8);
+    ASSERT_EQUAL(t.At(0), 8);
+    t.Back() = 7;
+    ASSERT_EQUAL(t[0], 7);
+    ASSERT_EQUAL(t.At(0), 7);
+
+}
+
+void MyTest() {
+    test_1();
+    test_2();
+    test_3();
+    test_4();
+    test_5();
+    test_6();
+    test_7();
+}
