79cae27cdc28f0b9be026f59155d685d352dc8b3
pavelzep <pavelzep@gmail.com>
2023-05-04 16:44:39 +0500

    v1

---
+++ 03_Red/01_Week/01_06_11_Paginator/src/paginator.h
@@ -0,0 +1,13 @@
+// Реализуйте шаблон класса Paginator
+
+template <typename Iterator>
+class Paginator {
+
+};
+
+// Реализуйте этот шаблон функции
+
+template <typename C>
+? ? ? Paginate(C & c, size_t page_size) {
+    
+}
