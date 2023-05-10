e50b39bba8164098fc1b522066b85b4090be626f
pavelzep <pavelzep@gmail.com>
2023-05-10 20:06:19 +0500

    v1

--- 03_Red/02_Week/02_01_07_new_language_learning/src/main.cpp
+++ 03_Red/02_Week/02_01_07_new_language_learning/src/main.cpp
@@ -2,24 +2,32 @@
 #include <sstream>
 #include <fstream>
 #include "learner.h"
+#include "profile.h"
 
 using namespace std;
 
 int main() {
+
     fstream cin("input.txt");
     Learner learner;
     string line;
-    while (getline(cin, line)) {
-        vector<string> words;
-        stringstream ss(line);
-        string word;
-        while (ss >> word) {
-            words.push_back(word);
+    {
+        LOG_DURATION("input");
+        while (getline(cin, line)) {
+            vector<string> words;
+            stringstream ss(line);
+            string word;
+            while (ss >> word) {
+                words.push_back(word);
+            }
+            cout << learner.Learn(words) << "\n";
         }
-        cout << learner.Learn(words) << "\n";
+        cout << "=== known words ===\n";
     }
-    cout << "=== known words ===\n";
-    for (auto word : learner.KnownWords()) {
-        cout << word << "\n";
+    {
+        LOG_DURATION("output");
+        for (auto word : learner.KnownWords()) {
+            cout << word << "\n";
+        }
     }
 }
