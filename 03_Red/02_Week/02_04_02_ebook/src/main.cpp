7412ceb9aae569f55e7098bc4b71dc318233760e
pavelzep <pavelzep@gmail.com>
2023-05-18 22:55:48 +0500

    v0

--- 03_Red/02_Week/02_04_02_ebook/src/main.cpp
+++ 03_Red/02_Week/02_04_02_ebook/src/main.cpp
@@ -2,39 +2,50 @@
 #include <iostream>
 #include <map>
 #include <set>
+#include <vector>
+#include <fstream>
 
 
 using namespace std;
 
 class ReadingManager {
 public:
-    ReadingManager() {
+    ReadingManager() :
+        user_count(0),
+        pages(1000, 0) {
 
     }
 
     void Read(int user, int page) {
+        auto it = user_to_page.find(user);
+        if (it == user_to_page.end()) {
+            ++user_count;
+            for (int i = it->second; i < 0; --i) {
+                pages[i]++;
+            }
+        } else {
+            for (int i = user_to_page[user]; i < page;++i) {
+                pages[i]++;
+            }
+        }
         user_to_page[user] = page;
-
-
-        page_to_user[page].insert(user);
-
-
     }
 
-    double Cheer(int user
-    ) const {
-
-
+    double Cheer(int user) const {
+        auto it = user_to_page.find(user);
+        if (it == user_to_page.end()) return {};
+        return pages[it->second] * 1.0 / user_count;
     }
 
 private:
-
-    map <int, set<int>> page_to_user;
+    int user_count;
     map <int, int> user_to_page;
+    vector <int> pages;
 };
 
 
 int main() {
+    fstream cin("../input.txt");
 
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
