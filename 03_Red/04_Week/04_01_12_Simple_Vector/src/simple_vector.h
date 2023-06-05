88b77235bb5c4587d8eb32208e2fcc28c713d9a8
pavezep <pavelzep@gmail.com>
2023-06-05 22:21:51 +0500

    v0

---
+++ 03_Red/04_Week/04_01_12_Simple_Vector/src/simple_vector.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <cstdlib>
+
+// Реализуйте шаблон SimpleVector
+template <typename T>
+class SimpleVector {
+public:
+    SimpleVector();
+    explicit SimpleVector(size_t size);
+    ~SimpleVector();
+
+    T& operator[](size_t index);
+
+    T* begin();
+    T* end();
+
+    size_t Size() const;
+    size_t Capacity() const;
+    void PushBack(const T& value);
+
+private:
+T* data
+    // Добавьте поля для хранения данных вектора
+};
+
+template<typename T>
+inline SimpleVector<T>::SimpleVector() {
+}
+
+template<typename T>
+inline SimpleVector<T>::SimpleVector(size_t size) {
+}
+
+template<typename T>
+inline SimpleVector<T>::~SimpleVector() {
+}
+
+template<typename T>
+inline T& SimpleVector<T>::operator[](size_t index) {
+    // TODO: вставьте здесь оператор return
+}
+
+template<typename T>
+inline T* SimpleVector<T>::begin() {
+    return nullptr;
+}
+
+template<typename T>
+inline T* SimpleVector<T>::end() {
+    return nullptr;
+}
+
+template<typename T>
+inline size_t SimpleVector<T>::Size() const {
+    return size_t();
+}
+
+template<typename T>
+inline size_t SimpleVector<T>::Capacity() const {
+    return size_t();
+}
+
+template<typename T>
+inline void SimpleVector<T>::PushBack(const T& value) {
+}
