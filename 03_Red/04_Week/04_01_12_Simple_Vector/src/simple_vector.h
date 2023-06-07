f225d81df0346d87259566e6583f088794a137bd
pavelzep <pavelzep@gmail.com>
2023-06-07 21:41:43 +0500

    v0

--- 03_Red/04_Week/04_01_12_Simple_Vector/src/simple_vector.h
+++ 03_Red/04_Week/04_01_12_Simple_Vector/src/simple_vector.h
@@ -26,20 +26,22 @@ private:
     T* data = nullptr;
     T* end_ = nullptr;
 
-    size_t size = 0;
-    size_t capacity = 0;
+    size_t size_ = 0;
+    size_t capacity_ = 0;
 
-
+    void reserve(size_t new_cap);
 };
 
 template<typename T>
-inline SimpleVector<T>::SimpleVector():size(0), capacity(0) {
+inline SimpleVector<T>::SimpleVector() :size_(0), capacity_(0) {
 }
 
 template<typename T>
-inline SimpleVector<T>::SimpleVector(size_t size): SimpleVector() {
+inline SimpleVector<T>::SimpleVector(size_t size) : SimpleVector() {
     data = new T[size];
     end_ = data + size;
+    size_ = size;
+    capacity_ = size;
 }
 
 template<typename T>
@@ -74,16 +76,37 @@ inline const T* SimpleVector<T>::end() const {
 
 template<typename T>
 inline size_t SimpleVector<T>::Size() const {
-    return size;
+    return size_;
 }
 
 template<typename T>
 inline size_t SimpleVector<T>::Capacity() const {
-    return capacity;
+    return capacity_;
 }
 
 template<typename T>
 inline void SimpleVector<T>::PushBack(const T& value) {
-     if(size==0)
-      
+    if (Size() == 0) {
+        reserve(1);
+    }
+    if (Size() == Capacity()) {
+        reserve(capacity_ * 2);
+    }
+    data[size_] = value;
+    size_++;
+    end_ = data + size_;
+}
+
+template<typename T>
+inline void SimpleVector<T>::reserve(size_t new_cap) {
+    if (new_cap <= capacity_) return;
+    T* new_data = new T[new_cap];
+
+    for (size_t i = 0; i < size_; ++i) {
+        new_data[i] = data[i];
+    }
+
+    delete[] data;
+    data = new_data;
+    capacity_ = new_cap;
 }
