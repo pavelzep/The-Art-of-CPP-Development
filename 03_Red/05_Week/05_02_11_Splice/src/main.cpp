9444a6b5dc1eaf564950b66f548b4ccda844da96
pavelzep <pavelzep@gmail.com>
2023-06-13 23:36:02 +0500

    v0

--- 03_Red/05_Week/05_02_11_Splice/src/main.cpp
+++ 03_Red/05_Week/05_02_11_Splice/src/main.cpp
@@ -1,3 +1,5 @@
+#include <list>
+
 template<typename T>
 class MyList {
     struct Node {
@@ -6,20 +8,47 @@ class MyList {
         Node* next = nullptr;
     };
 
+
     ~MyList();
 
 
-private:
+    void push_back
+
     Node* head = nullptr;
+    Node* tail = nullptr;
+
+private:
 
 };
+
+template<typename T>
+MyList<T>::~MyList() {
+}
+
+template<typename T>
+MyList<T>& operator<<(MyList<T>& lst, const T& value) {
+    auto new_tail = new Node{ value };
+
+    if (lst.head == nullptr) {
+        lst.head = new_tail;
+        lst.tail = new_tail;
+    } else {
+        lst.tail->next = new_tail;
+        new_tail->prev = lst.tail;
+        lst.tail = new_tail;
+    }
+    return lst;
+};
+
 
 
 
 int main() {
 
-
+    MyList<int> lst;
+    lst << 1 << 2 << 3 << 4 << 5;
 
     return 0;
 }
+
 
