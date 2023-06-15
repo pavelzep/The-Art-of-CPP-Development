e47258bd62b0066d11fe2129c8d463b7d82ad63a
pavelzep <pavelzep@gmail.com>
2023-06-15 23:54:25 +0500

    accepted

--- 03_Red/05_Week/05_02_11_Splice/src/main.cpp
+++ 03_Red/05_Week/05_02_11_Splice/src/main.cpp
@@ -4,12 +4,33 @@
 
 int main() {
 
-    SimpleList<int> lst;
-    for (int i = 0;i < 10;++i) {
-        lst.PushBack(i);
-    }
+    SimpleList<int> lst1;
+    for (int i = 0;i < 10;++i) lst1.PushBack(i);
 
-    lst.Print();
+    lst1.Print();
+    std::cout << std::endl;
+
+    SimpleList<int> lst2;
+    for (int i = 10;i < 20;++i) lst2.PushBack(i);
+
+    lst2.Print();
+    std::cout << std::endl;
+
+    auto pos = lst2.Head()->next->next->next; //13
+    std::cout << pos->value;
+    std::cout << std::endl;
+
+    auto first = lst1.Head()->next->next->next;//
+    auto last = first->next->next->next;
+    std::cout << first->value << ' ' << last->prev->value << std::endl;
+
+
+    lst2.Splice(pos, lst2, first, last);
+    lst1.Print();
+    std::cout << std::endl;
+    lst2.Print();
+    std::cout << std::endl;
+
 
     return 0;
 }
--- 03_Red/05_Week/05_02_11_Splice/src/simple_list.h
+++ 03_Red/05_Week/05_02_11_Splice/src/simple_list.h
@@ -2,6 +2,7 @@
 
 #pragma once
 #include <iostream>
+#include <iterator>
 
 template<typename T>
 class SimpleList {
@@ -16,9 +17,14 @@ class SimpleList {
 
 public:
 
+    Node* Head() { return head; }
+    Node* Next(Node* it) { return it->next; }
+    Node* Prev(Node* it) { return it->prev; }
+
     ~SimpleList();
     void PushBack(const T& value);
     void Print();
+    void Splice(Node* pos, SimpleList& other, Node* first, Node* last);
 };
 
 template<typename T>
@@ -47,6 +53,25 @@ inline void SimpleList<T>::Print() {
         std::cout << it->value << ' ';
         it = it->next;
     } while (it != nullptr);
+}
+
+template<typename T>
+inline void SimpleList<T>::Splice(Node* pos, SimpleList& other, Node* first, Node* last) {
+    // pos->prev->next = first; 
+    // pos->prev = last->prev;
+    // first->prev = pos->prev;
+    // last->prev->next = pos;
+    // first->prev->next = last;
+    // last->prev = first->prev;
+
+    auto temp = pos->prev;
+    pos->prev->next = first;    //1
+    first->prev->next = last; //5
+    last->prev->next = pos; //4
+    pos->prev = last->prev; //2
+    last->prev = first->prev; //6
+    first->prev = temp; //3
+
 }
 
 
