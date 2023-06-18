6a54fb907c9623a71f806542f651508f741a4338
pavezep <pavelzep@gmail.com>
2023-06-18 18:43:04 +0500

    v0

--- 03_Red/05_Week/05_03_01_StackVector/src/stack_vector.h
+++ 03_Red/05_Week/05_03_01_StackVector/src/stack_vector.h
@@ -1,27 +1,36 @@
 #pragma once
-
+#include <array>
 #include <stdexcept>
 using namespace std;
 
 template <typename T, size_t N>
 class StackVector {
 public:
-  explicit StackVector(size_t a_size = 0);
+    explicit StackVector(size_t a_size = 0) {
+        
+    }
 
-  T& operator[](size_t index);
-  const T& operator[](size_t index) const;
+    T& operator[](size_t index) { return store[index]; }
+    const T& operator[](size_t index) const { return store[index]; }
 
-  ??? begin();
-  ??? end();
-  ??? begin() const;
-  ??? end() const;
+    auto begin() { return &store[0]; }
+    auto end() { return &store[0]; }
+    auto begin() const { return &store[0]; }
+    auto end() const { return &store[0]; }
 
-  size_t Size() const;
-  size_t Capacity() const;
+    size_t Size() const {
+        return size_t();
+    }
+    size_t Capacity() const { return N; }
 
-  void PushBack(const T& value);
-  T PopBack();
+    void PushBack(const T& value) {
+
+    };
+    T PopBack() {
+        return T();
+    }
 
 private:
-};
+    array<T, N> store;
 
+};
