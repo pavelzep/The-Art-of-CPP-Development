376fda6106458441ebfd2c6eb40196333349ff2b
pavelzep <pavelzep@gmail.com>
2023-07-01 00:09:23 +0500

    v0

--- 03_Red/05_Week/05_03_12_WebServer/src/stats.h
+++ 03_Red/05_Week/05_03_12_WebServer/src/stats.h
@@ -8,10 +8,20 @@ using namespace std;
 
 class Stats {
 public:
-  void AddMethod(string_view method);
-  void AddUri(string_view uri);
-  const map<string_view, int>& GetMethodStats() const;
-  const map<string_view, int>& GetUriStats() const;
+    void AddMethod(string_view method){
+
+    }
+    void AddUri(string_view uri){
+
+    }
+    const map<string_view, int>& GetMethodStats() const{
+
+    }
+    const map<string_view, int>& GetUriStats() const{
+
+    }
 };
 
-HttpRequest ParseRequest(string_view line);
+HttpRequest ParseRequest(string_view line){
+
+}
