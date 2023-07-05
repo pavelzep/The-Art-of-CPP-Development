234be627d54860d2485fef67cc24c4903489d923
pavelzep <pavelzep@gmail.com>
2023-07-05 14:23:59 +0500

    v1

--- 03_Red/06_Week/06_01_07_josephs_permutation/src/main.cpp
+++ 03_Red/06_Week/06_01_07_josephs_permutation/src/main.cpp
@@ -4,41 +4,48 @@
 #include <iterator>
 #include <numeric>
 #include <vector>
+#include <utility>
 
 using namespace std;
 
 template <typename RandomIt>
 void MakeJosephusPermutation(RandomIt first, RandomIt last, uint32_t step_size) {
-  vector<typename RandomIt::value_type> pool(first, last);
-  size_t cur_pos = 0;
-  while (!pool.empty()) {
-    *(first++) = pool[cur_pos];
-    pool.erase(pool.begin() + cur_pos);
-    if (pool.empty()) {
-      break;
+    vector<typename RandomIt::value_type> pool;
+    auto it = first;
+    while (it != last) {
+        pool.push_back(move(*it));
+        ++it;
     }
-    cur_pos = (cur_pos + step_size - 1) % pool.size();
-  }
+
+    size_t cur_pos = 0;
+    while (!pool.empty()) {
+        *(first++) = move(pool[cur_pos]);
+        pool.erase(pool.begin() + cur_pos);
+        if (pool.empty()) {
+            break;
+        }
+        cur_pos = (cur_pos + step_size - 1) % pool.size();
+    }
 }
 
 vector<int> MakeTestVector() {
-  vector<int> numbers(10);
-  iota(begin(numbers), end(numbers), 0);
-  return numbers;
+    vector<int> numbers(10);
+    iota(begin(numbers), end(numbers), 0);
+    return numbers;
 }
 
 void TestIntVector() {
-  const vector<int> numbers = MakeTestVector();
-  {
-    vector<int> numbers_copy = numbers;
-    MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 1);
-    ASSERT_EQUAL(numbers_copy, numbers);
-  }
-  {
-    vector<int> numbers_copy = numbers;
-    MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 3);
-    ASSERT_EQUAL(numbers_copy, vector<int>({0, 3, 6, 9, 4, 8, 5, 2, 7, 1}));
-  }
+    const vector<int> numbers = MakeTestVector();
+    {
+        vector<int> numbers_copy = numbers;
+        MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 1);
+        ASSERT_EQUAL(numbers_copy, numbers);
+    }
+    {
+        vector<int> numbers_copy = numbers;
+        MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 3);
+        ASSERT_EQUAL(numbers_copy, vector<int>({ 0, 3, 6, 9, 4, 8, 5, 2, 7, 1 }));
+    }
 }
 
 // Это специальный тип, который поможет вам убедиться, что ваша реализация
@@ -48,46 +55,46 @@ void TestIntVector() {
 // в видео «Некопируемые типы»
 
 struct NoncopyableInt {
-  int value;
+    int value;
 
-  NoncopyableInt(const NoncopyableInt&) = delete;
-  NoncopyableInt& operator=(const NoncopyableInt&) = delete;
+    NoncopyableInt(const NoncopyableInt&) = delete;
+    NoncopyableInt& operator=(const NoncopyableInt&) = delete;
 
-  NoncopyableInt(NoncopyableInt&&) = default;
-  NoncopyableInt& operator=(NoncopyableInt&&) = default;
+    NoncopyableInt(NoncopyableInt&&) = default;
+    NoncopyableInt& operator=(NoncopyableInt&&) = default;
 };
 
 bool operator == (const NoncopyableInt& lhs, const NoncopyableInt& rhs) {
-  return lhs.value == rhs.value;
+    return lhs.value == rhs.value;
 }
 
 ostream& operator << (ostream& os, const NoncopyableInt& v) {
-  return os << v.value;
+    return os << v.value;
 }
 
 void TestAvoidsCopying() {
-  vector<NoncopyableInt> numbers;
-  numbers.push_back({1});
-  numbers.push_back({2});
-  numbers.push_back({3});
-  numbers.push_back({4});
-  numbers.push_back({5});
+    vector<NoncopyableInt> numbers;
+    numbers.push_back({ 1 });
+    numbers.push_back({ 2 });
+    numbers.push_back({ 3 });
+    numbers.push_back({ 4 });
+    numbers.push_back({ 5 });
 
-  MakeJosephusPermutation(begin(numbers), end(numbers), 2);
+    MakeJosephusPermutation(begin(numbers), end(numbers), 2);
 
-  vector<NoncopyableInt> expected;
-  expected.push_back({1});
-  expected.push_back({3});
-  expected.push_back({5});
-  expected.push_back({4});
-  expected.push_back({2});
+    vector<NoncopyableInt> expected;
+    expected.push_back({ 1 });
+    expected.push_back({ 3 });
+    expected.push_back({ 5 });
+    expected.push_back({ 4 });
+    expected.push_back({ 2 });
 
-  ASSERT_EQUAL(numbers, expected);
+    ASSERT_EQUAL(numbers, expected);
 }
 
 int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestIntVector);
-  RUN_TEST(tr, TestAvoidsCopying);
-  return 0;
+    TestRunner tr;
+    RUN_TEST(tr, TestIntVector);
+    RUN_TEST(tr, TestAvoidsCopying);
+    return 0;
 }
