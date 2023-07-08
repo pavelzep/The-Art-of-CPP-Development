11e7c1d1d5ce418cf9ed37c7ae131fe31f894457
pavezep <pavelzep@gmail.com>
2023-07-08 18:02:14 +0500

    v1

--- 03_Red/06_Week/06_01_08_group_strings/src/main.cpp
+++ 03_Red/06_Week/06_01_08_group_strings/src/main.cpp
@@ -3,6 +3,9 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <map>
+#include <set>
+#include <utility>
 
 using namespace std;
 
@@ -22,34 +25,49 @@ using Char = typename String::value_type;
 
 template <typename String>
 vector<Group<String>> GroupHeavyStrings(vector<String> strings) {
-  // Напишите реализацию функции,
-  // использовав не более 1 копирования каждого символа
+    // Напишите реализацию функции,
+    // использовав не более 1 копирования каждого символа
+
+    set<typename String::value_type> key;
+
+    map< set <Char<String>>, Group<String>> mapStrings;
+    for (const String& item : strings) {
+        set<Char<String>> key(make_move_iterator(begin(item)), make_move_iterator(end(item)));
+        mapStrings[move(key)].push_back(move(item));
+    }
+
+    vector<Group<String>> result;
+    for (const auto& item : mapStrings) {
+        result.push_back(move(item.second));
+    }
+
+    return result;
 }
 
 
 void TestGroupingABC() {
-  vector<string> strings = {"caab", "abc", "cccc", "bacc", "c"};
-  auto groups = GroupHeavyStrings(strings);
-  ASSERT_EQUAL(groups.size(), 2);
-  sort(begin(groups), end(groups));  // Порядок групп не имеет значения
-  ASSERT_EQUAL(groups[0], vector<string>({"caab", "abc", "bacc"}));
-  ASSERT_EQUAL(groups[1], vector<string>({"cccc", "c"}));
+    vector<string> strings = { "caab", "abc", "cccc", "bacc", "c" };
+    auto groups = GroupHeavyStrings(strings);
+    ASSERT_EQUAL(groups.size(), 2);
+    sort(begin(groups), end(groups));  // Порядок групп не имеет значения
+    ASSERT_EQUAL(groups[0], vector<string>({ "caab", "abc", "bacc" }));
+    ASSERT_EQUAL(groups[1], vector<string>({ "cccc", "c" }));
 }
 
 void TestGroupingReal() {
-  vector<string> strings = {"law", "port", "top", "laptop", "pot", "paloalto", "wall", "awl"};
-  auto groups = GroupHeavyStrings(strings);
-  ASSERT_EQUAL(groups.size(), 4);
-  sort(begin(groups), end(groups));  // Порядок групп не имеет значения
-  ASSERT_EQUAL(groups[0], vector<string>({"laptop", "paloalto"}));
-  ASSERT_EQUAL(groups[1], vector<string>({"law", "wall", "awl"}));
-  ASSERT_EQUAL(groups[2], vector<string>({"port"}));
-  ASSERT_EQUAL(groups[3], vector<string>({"top", "pot"}));
+    vector<string> strings = { "law", "port", "top", "laptop", "pot", "paloalto", "wall", "awl" };
+    auto groups = GroupHeavyStrings(strings);
+    ASSERT_EQUAL(groups.size(), 4);
+    sort(begin(groups), end(groups));  // Порядок групп не имеет значения
+    ASSERT_EQUAL(groups[0], vector<string>({ "laptop", "paloalto" }));
+    ASSERT_EQUAL(groups[1], vector<string>({ "law", "wall", "awl" }));
+    ASSERT_EQUAL(groups[2], vector<string>({ "port" }));
+    ASSERT_EQUAL(groups[3], vector<string>({ "top", "pot" }));
 }
 
 int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestGroupingABC);
-  RUN_TEST(tr, TestGroupingReal);
-  return 0;
+    TestRunner tr;
+    RUN_TEST(tr, TestGroupingABC);
+    RUN_TEST(tr, TestGroupingReal);
+    return 0;
 }
