5137288c5d60121cb628174a9fb57facb033348c
pavelzep <pavelzep@gmail.com>
2023-07-14 20:09:09 +0500

    accepted

--- 03_Red/06_Week/06_02_12_simple_vector_2/src/simple_vector_2.h
+++ 03_Red/06_Week/06_02_12_simple_vector_2/src/simple_vector_2.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <cstdint>
 #include <iostream>
 
@@ -14,15 +16,16 @@ public:
 
     T& operator[](size_t index);
     void operator=(const SimpleVector& other);
+    void operator=(SimpleVector&& other);
 
-    T* begin();
-    T* end();
+    T* begin() { return data; }
+    T* end() { return data + size; }
 
-    const T* begin() const;
-    const T* end() const;
+    const T* begin() const { return data; }
+    const T* end()  const { return data + size; }
 
-    size_t Size() const;
-    size_t Capacity() const;
+    size_t Size() const { return size; }
+    size_t Capacity() const { return capacity; }
     void PushBack(const T& value);
     void PushBack(T&& value);
 
@@ -34,16 +37,11 @@ private:
     void reserve(size_t new_cap);
 };
 
-
-// template<typename T>
-// inline SimpleVector<T>::SimpleVector() :size(0), capacity(0) {
-// }
-
 template<typename T>
-inline SimpleVector<T>::SimpleVector(size_t _size) : SimpleVector() {
-    data = new T[_size];
-    size = _size;
-    capacity = _size;
+inline SimpleVector<T>::SimpleVector(size_t _size)
+    : data(new T[_size])
+    , size(_size)
+    , capacity(_size) {
 }
 
 template<typename T>
@@ -55,7 +53,19 @@ inline SimpleVector<T>::SimpleVector(const SimpleVector& other) :
 }
 
 template<typename T>
-inline SimpleVector<T>::SimpleVector(SimpleVector&& other) {
+inline SimpleVector<T>::SimpleVector(SimpleVector&& other) :
+    size(other.size),
+    capacity(other.capacity) {
+    delete[] data;
+    move(other.begin(), other.end(), begin());
+}
+
+template<typename T>
+inline void SimpleVector<T>::operator=(SimpleVector&& other) {
+    delete[] data;
+    data = other.data;
+    size = other.size;
+    capacity = other.capacity;
 }
 
 template<typename T>
@@ -75,39 +85,6 @@ inline void SimpleVector<T>::operator=(const SimpleVector& other) {
     size = other.size;
     capacity = other.capacity;
     copy(other.begin(), other.end(), begin());
-    // for (size_t i = 0; i < capacity; i++) {
-    //     data[i] = other.data[i];
-    // }
-}
-
-template<typename T>
-inline T* SimpleVector<T>::begin() {
-    return data;
-}
-
-template<typename T>
-inline T* SimpleVector<T>::end() {
-    return data + size;
-}
-
-template<typename T>
-inline const T* SimpleVector<T>::begin() const {
-    return data;
-}
-
-template<typename T>
-inline const T* SimpleVector<T>::end() const {
-    return data + size;
-}
-
-template<typename T>
-inline size_t SimpleVector<T>::Size() const {
-    return size;
-}
-
-template<typename T>
-inline size_t SimpleVector<T>::Capacity() const {
-    return capacity;
 }
 
 template<typename T>
@@ -124,10 +101,15 @@ inline void SimpleVector<T>::PushBack(const T& value) {
 
 template<typename T>
 inline void SimpleVector<T>::PushBack(T&& value) {
+    if (Size() == 0) {
+        reserve(1);
+    }
+    if (Size() == Capacity()) {
+        reserve(capacity * 2);
+    }
 
-    
-
-
+    data[size] = move(value);
+    size++;
 }
 
 template<typename T>
@@ -136,7 +118,7 @@ inline void SimpleVector<T>::reserve(size_t new_cap) {
     T* new_data = new T[new_cap];
 
     for (size_t i = 0; i < size; ++i) {
-        new_data[i] = data[i];
+        new_data[i] = move(data[i]);
     }
 
     delete[] data;
