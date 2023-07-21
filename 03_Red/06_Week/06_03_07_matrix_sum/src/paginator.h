0f74b4005bd956c5ea1771e8075781ab12c2c208
pavelzep <pavelzep@gmail.com>
2023-07-21 20:40:28 +0500

    v0

---
+++ 03_Red/06_Week/06_03_07_matrix_sum/src/paginator.h
@@ -0,0 +1,45 @@
+#include <vector>
+using namespace std;
+
+template<typename Iterator>
+class IteratorRange {
+private:
+    Iterator first, last;
+
+public:
+    IteratorRange(Iterator f, Iterator l)
+        : first(f)
+        , last(l) {
+    }
+
+    Iterator begin() const { return first; }
+
+    Iterator end() const { return last; }
+
+    size_t size() const { return end() - begin(); }
+};
+
+template <typename Iterator>
+class Paginator {
+private:
+    vector <IteratorRange<Iterator>> pages;
+
+public:
+    Paginator(Iterator begin, Iterator end, size_t page_size) {
+        for (Iterator it = begin;it < end;it = next(it, page_size)) {
+            IteratorRange page(it, min(next(it, page_size), end));
+            pages.push_back(page);
+        }
+    }
+
+    size_t size() const { return pages.size(); }
+
+    auto begin() const { return pages.begin(); }
+
+    auto end() const { return pages.end(); }
+};
+
+template <typename Container>
+auto Paginate(Container& container, size_t page_size) {
+    return Paginator(container.begin(), container.end(), page_size);
+};
