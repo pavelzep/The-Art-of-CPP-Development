84d38b734d8e4e7a397f38ef50de47dfeabd1ea8
pavelzep <pavelzep@gmail.com>
2023-07-25 13:17:29 +0500

    v0

--- 03_Red/06_Week/06_03_08_explore_key_words/src/explore_key_words.h
+++ 03_Red/06_Week/06_03_08_explore_key_words/src/explore_key_words.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <set>
 #include <cctype>
+#include <string_view>
 
 
 using namespace std;
@@ -14,18 +15,40 @@ struct Stats {
 };
 
 Stats ExploreLine(const set<string>& key_words, const string& line) {
-    size_t pos = 0
-        for (const auto& key : key_words) {
-            n = line.find(key, pos);
-            if (n != string::npos) {
-                size_t t = n + key.size();
-                if(isspace(line[t].)){
-                    
-                }
+
+    /*
+    size_t pos = 0;
+    for (const auto& key : key_words) {
+        n = line.find(key, pos);
+        if (n != string::npos) {
+            size_t t = n + key.size();
+            if(isspace(line[t].)){
+            }
+        }
+    }
+    */
+    Stats result;
+    for (const auto& key : key_words) {
+        string_view find_words;
+        string_view key_view = key;
+        size_t pos = 0;
+        const size_t pos_end = line.npos;
+        while (true) {
+            size_t space_pos = line.find(' ', pos);
+            find_words = space_pos == pos_end ? line.substr(pos) : line.substr(pos, space_pos - pos);
+            if (find_words == key_view) {
+                result.word_frequences[key] += 1;
             }
 
+            if (space_pos == pos_end) {
+                break;
+            } else {
+                pos = space_pos + 1;
+            }
         }
-    Stats();
+
+    }
+    return result;
 }
 
 Stats ExploreKeyWordsSingleThread(
@@ -39,9 +62,15 @@ Stats ExploreKeyWordsSingleThread(
 }
 
 Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
-    // Реализуйте эту функцию
-    Stats();
+
+    
+
+
+    return ExploreKeyWordsSingleThread(key_words, input);
 }
 
 void Stats::operator+=(const Stats& other) {
+    for (auto& item : other.word_frequences) {
+        word_frequences[item.first] += item.second;
+    }
 }
