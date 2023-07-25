34f8e92f0ada8a0e72b03227858db562c2ca4476
pavelzep <pavelzep@gmail.com>
2023-07-25 16:47:48 +0500

    v0

--- 03_Red/06_Week/06_03_08_explore_key_words/src/explore_key_words.h
+++ 03_Red/06_Week/06_03_08_explore_key_words/src/explore_key_words.h
@@ -5,9 +5,55 @@
 #include <set>
 #include <cctype>
 #include <string_view>
+#include <future>
+#include <functional>
+#define THREAD_COUNT 4
 
 
 using namespace std;
+
+
+
+
+template<typename Iterator>
+class IteratorRange {
+private:
+    Iterator first, last;
+public:
+    IteratorRange(Iterator f, Iterator l)
+        : first(f)
+        , last(l) {
+    }
+    Iterator begin() const { return first; }
+    Iterator end() const { return last; }
+    size_t size() const { return end() - begin(); }
+};
+
+template <typename Iterator>
+class Paginator {
+private:
+    vector <IteratorRange<Iterator>> pages;
+public:
+    Paginator(Iterator begin, Iterator end, size_t page_size) {
+        for (Iterator it = begin;it < end;it = next(it, page_size)) {
+            IteratorRange page(it, min(next(it, page_size), end));
+            pages.push_back(page);
+        }
+    }
+    size_t size() const { return pages.size(); }
+    auto begin() const { return pages.begin(); }
+    auto end() const { return pages.end(); }
+};
+
+template <typename Container>
+auto Paginate(Container& container, size_t page_size) {
+    return Paginator(container.begin(), container.end(), page_size);
+};
+
+
+
+
+
 
 struct Stats {
     map<string, int> word_frequences;
@@ -34,6 +80,7 @@ Stats ExploreLine(const set<string>& key_words, const string& line) {
         size_t pos = 0;
         const size_t pos_end = line.npos;
         while (true) {
+
             size_t space_pos = line.find(' ', pos);
             find_words = space_pos == pos_end ? line.substr(pos) : line.substr(pos, space_pos - pos);
             if (find_words == key_view) {
@@ -63,10 +110,22 @@ Stats ExploreKeyWordsSingleThread(
 
 Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
 
-    
+    // futures_stats.push_back(async(ExploreKeyWordsSingleThread, key_words, input));
+    Stats result;
+    vector<future<Stats>> futures_stats;
+    const size_t thread_count = key_words.size() < 4 ? 1 : THREAD_COUNT;
+    const size_t page_size = key_words.size() / thread_count;
+
+    for (const auto page : Paginate(key_words, page_size)) {
+        futures_stats.push_back(async(ExploreKeyWordsSingleThread, ref(key_words), ref(input)));
+    }
 
 
-    return ExploreKeyWordsSingleThread(key_words, input);
+    for (auto& item : futures_stats) {
+        result += item.get();
+    }
+    return result;
+    // return async(ExploreKeyWordsSingleThread, ref(key_words), ref(input)).get();
 }
 
 void Stats::operator+=(const Stats& other) {
