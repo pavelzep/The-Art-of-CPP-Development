4bb961dbc3ed3ed3f833219acf7096bfa9edfd90
pavelzep <pavelzep@gmail.com>
2023-08-15 22:55:30 +0500

    v0

--- 03_Red/06_Week/06_04_01_concurrent_map/src/concurrent_map.h
+++ 03_Red/06_Week/06_04_01_concurrent_map/src/concurrent_map.h
@@ -4,8 +4,9 @@
 #include <algorithm>
 #include <numeric>
 #include <random>
-#include <future>
+
 #include <map>
+#include <mutex>
 
 using namespace std;
 
@@ -15,36 +16,48 @@ public:
     static_assert(is_integral_v<K>, "ConcurrentMap supports only integer keys");
 
     struct Access {
+        // lock_guard <mutex> guard;
         V& ref_to_value;
     };
 
+    // struct Bucket {
+    //     map <K, V> bucket_map;
+    //     mutex m;
+    // };
+
     explicit ConcurrentMap(size_t _bucket_count) :
-        bucket_count(_bucket_count) {
+        bucket_count(_bucket_count),
+        current_bucket_index(0) {
+        storage.reserve(bucket_count);
+        for (size_t i = 0; i < bucket_count; ++i) {
+            storage.push_back(map<K, V>());
+
+        }
 
     }
 
     Access operator[](const K& key) {
-        for (size_t i = 0; i < current_bucket_index; ++i) {
+        for (size_t i = 0; i < bucket_count; ++i) {
 
             if (storage[i].count(key)) {
                 return { storage[i][key] };
             }
-
-            return { storage[current_bucket_index][key] };
-
+            current_bucket_index_update();
+            return { storage[++current_bucket_index][key] };
 
         }
     }
-    map<K, V> BuildOrdinaryMap() {
 
+    map<K, V> BuildOrdinaryMap() {
     }
+
 private:
-    vector<map< K, V>> storage;
+    vector< map <K, V>> storage;
+    // vector<mutex> mutex_vec;
     size_t bucket_count;
     size_t current_bucket_index;
 
-    void indexUpdate() {
-        current_bucket_index++;
+    void current_bucket_index_update() {
         if (current_bucket_index == bucket_count) current_bucket_index = 0;
     }
 };
--- 03_Red/06_Week/06_04_01_concurrent_map/src/tests.h
+++ 03_Red/06_Week/06_04_01_concurrent_map/src/tests.h
@@ -6,6 +6,7 @@
 
 #include <vector>
 #include <string>
+#include <future>
 
 
 void TestAll();
