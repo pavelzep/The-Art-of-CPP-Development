9d5597c575122b1d5994fa96b5cd05cb8c61466e
pavelzep <pavelzep@gmail.com>
2023-08-17 21:58:13 +0500

    v0

--- 03_Red/06_Week/06_04_01_concurrent_map/src/concurrent_map.h
+++ 03_Red/06_Week/06_04_01_concurrent_map/src/concurrent_map.h
@@ -21,60 +21,49 @@ public:
         V& ref_to_value;
     };
 
-    // struct Bucket{
-    //     unique_ptr<mutex> mutex_ptr;
-    //     map <K, V> bucket;
-    // };
-
-
     explicit ConcurrentMap(size_t _bucket_count) :
         bucket_count(_bucket_count),
         current_bucket_index(0) {
 
         for (size_t i = 0; i < bucket_count; ++i) {
             storage.push_back(map<K, V>());
-            // store.push_back({make_unique<std::mutex>(),map<K, V>()});
+            mutexes.push_back(make_unique<std::mutex>());
+
         }
     }
 
     Access operator[](const K& key) {
         for (size_t i = 0; i < bucket_count; ++i) {
             if (storage[i].count(key)) {
-                mutex m;
-                return { lock_guard(m), storage[i][key] };
+                return { lock_guard(*mutexes[i]), storage[i][key] };
             }
         }
 
         {
-            mutex m;
-            return { lock_guard(m),storage[current_bucket_index_update()][key] };
+            lock_guard g(self_mutex);
+            size_t ind = current_bucket_index_update();
+            return { lock_guard(*mutexes[ind]),storage[ind][key] };
         }
     }
 
     map<K, V> BuildOrdinaryMap() {
+        lock_guard g(self_mutex);
         map<K, V> result;
-        // for (auto& item : storage) {
-        //     result.merge(item);
-        // }
-
-        for(size_t i = 0;  i < bucket_count; ++i ){
-            
-storage[i].
-
-
+        for (auto& item : storage) {
+            lock_guard b_m(bucket_mutex);
+            result.merge(item);
         }
-    
 
-
+        // for (size_t i = 0; i < bucket_count; ++i) {}
         return result;
     }
 
 private:
     vector< map <K, V>> storage;
-    // vector <Bucket> store;
-    size_t bucket_count;
+    vector <unique_ptr<mutex>>mutexes;
     size_t current_bucket_index;
-    
+    size_t bucket_count;
+    mutex self_mutex, bucket_mutex;
 
     size_t current_bucket_index_update() {
         current_bucket_index++;
