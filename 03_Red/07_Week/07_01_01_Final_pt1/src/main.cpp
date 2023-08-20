cc23a4c6a15ca51dfe9e0bf862f75d313afa318c
pavelzep <pavelzep@gmail.com>
2023-08-20 19:50:14 +0500

    v0

---
+++ 03_Red/07_Week/07_01_01_Final_pt1/src/main.cpp
@@ -0,0 +1,19 @@
+
+
+#include "tests.h"
+
+// #include <algorithm>
+// #include <iterator>
+// #include <map>
+// #include <vector>
+// #include <string>
+// #include <sstream>
+// #include <fstream>
+// #include <random>
+// #include <thread>
+// using namespace std;
+
+
+int main() {
+    TestAll();
+}
