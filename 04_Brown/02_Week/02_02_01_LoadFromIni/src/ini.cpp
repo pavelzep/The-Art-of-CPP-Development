785129ccba77f33b6ec7c475863c177a885992ff
pavelzep <pavelzep@gmail.com>
2024-01-31 19:46:19 +0500

    modified:   04_Brown/02_Week/02_02_01_LoadFromIni/src/ini.cpp

--- 04_Brown/02_Week/02_02_01_LoadFromIni/src/ini.cpp
+++ 04_Brown/02_Week/02_02_01_LoadFromIni/src/ini.cpp
@@ -1,6 +1,5 @@
 #include "ini.h"
 
-
 namespace Ini {
 
     Section& Document::AddSection(string name) {
@@ -9,7 +8,9 @@ namespace Ini {
     }
 
     const Section& Document::GetSection(const string& name) const {
-        return (*(sections.find(name))).second;
+        auto it = sections.find(name);
+        const Section& result = it!= sections.end()? (*it).second: throw out_of_range("Wrong section name");
+         return result;
     }
 
     size_t Document::SectionCount() const {
@@ -18,25 +19,21 @@ namespace Ini {
 
     Document Load(istream& input) {
         Document result;
-        Section section;
-
+        Section* section = nullptr;
         for (string line; getline(input, line);) {
             if (!line.empty()) {
                 if (line[0] == '[') {
                     string section_name = line.substr(1, line.size() - 2);
-                    section = result.AddSection(section_name);             
+                    section = &result.AddSection(section_name);
                 } else {
                     auto pos = line.find_first_of('=');
-                    string l = line.substr(0,pos);
-                    string r = line.substr(pos+1, line.size()-(pos));
-                    section.insert(pair<string,string>(move(l),move(r)));    
-                            
+                    string l = line.substr(0, pos);
+                    string r = line.substr(pos + 1, line.size() - (pos));
+                    section->insert(pair<string, string>(move(l), move(r)));
                 }
             }
 
         }
-
-
         return result;
     }
 }
