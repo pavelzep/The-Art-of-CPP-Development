7a6da277e41d77ce7d8f2baf7c629f0715ad1400
pavelzep <pavelzep@gmail.com>
2024-02-06 13:45:55 +0500

    modified:   04_Brown/02_Week/02_03_06_Collide/CMakeLists.txt 	modified:   04_Brown/02_Week/02_03_06_Collide/src/collide.cpp 	new file:   04_Brown/02_Week/02_03_06_Collide/src/main.cpp 	new file:   04_Brown/02_Week/02_03_06_Collide/src/tests.h

---
+++ 04_Brown/02_Week/02_03_06_Collide/src/tests.h
@@ -0,0 +1,67 @@
+#include "test_runner.h"
+#include "game_object.h"
+#include "geo2d.h"
+
+#include <memory>
+#include <vector>
+
+using namespace std;
+
+
+void TestAll();
+void TestAddingNewObjectOnMap();
+
+void TestAll() {
+    TestRunner tr;
+    RUN_TEST(tr, TestAddingNewObjectOnMap);
+}
+
+void TestAddingNewObjectOnMap() {
+    // Юнит-тест моделирует ситуацию, когда на игровой карте уже есть какие-то объекты,
+    // и мы хотим добавить на неё новый, например, построить новое здание или башню.
+    // Мы можем его добавить, только если он не пересекается ни с одним из существующих.
+    using namespace geo2d;
+
+    const vector<shared_ptr<GameObject>> game_map = {
+      make_shared<Unit>(Point{3, 3}),
+      make_shared<Unit>(Point{5, 5}),
+      make_shared<Unit>(Point{3, 7}),
+      make_shared<Fence>(Segment{{7, 3}, {9, 8}}),
+      make_shared<Tower>(Circle{Point{9, 4}, 1}),
+      make_shared<Tower>(Circle{Point{10, 7}, 1}),
+      make_shared<Building>(Rectangle{{11, 4}, {14, 6}})
+    };
+
+    for (size_t i = 0; i < game_map.size(); ++i) {
+        Assert(
+            Collide(*game_map[i], *game_map[i]),
+            "An object doesn't collide with itself: " + to_string(i)
+        );
+
+        for (size_t j = 0; j < i; ++j) {
+            Assert(
+                !Collide(*game_map[i], *game_map[j]),
+                "Unexpected collision found " + to_string(i) + ' ' + to_string(j)
+            );
+        }
+    }
+
+    auto new_warehouse = make_shared<Building>(Rectangle{ {4, 3}, {9, 6} });
+    ASSERT(!Collide(*new_warehouse, *game_map[0]));
+    ASSERT(Collide(*new_warehouse, *game_map[1]));
+    ASSERT(!Collide(*new_warehouse, *game_map[2]));
+    ASSERT(Collide(*new_warehouse, *game_map[3]));
+    ASSERT(Collide(*new_warehouse, *game_map[4]));
+    ASSERT(!Collide(*new_warehouse, *game_map[5]));
+    ASSERT(!Collide(*new_warehouse, *game_map[6]));
+
+    auto new_defense_tower = make_shared<Tower>(Circle{ {8, 2}, 2 });
+    ASSERT(!Collide(*new_defense_tower, *game_map[0]));
+    ASSERT(!Collide(*new_defense_tower, *game_map[1]));
+    ASSERT(!Collide(*new_defense_tower, *game_map[2]));
+    ASSERT(Collide(*new_defense_tower, *game_map[3]));
+    ASSERT(Collide(*new_defense_tower, *game_map[4]));
+    ASSERT(!Collide(*new_defense_tower, *game_map[5]));
+    ASSERT(!Collide(*new_defense_tower, *game_map[6]));
+}
+
