28ffcdbc8416f1cf0e6e21cdd8fe045dcdaf93be
pavelzep <pavelzep@gmail.com>
2024-02-07 22:43:17 +0500

    modified:   04_Brown/02_Week/02_03_06_Collide/src/collide.cpp

--- 04_Brown/02_Week/02_03_06_Collide/src/collide.cpp
+++ 04_Brown/02_Week/02_03_06_Collide/src/collide.cpp
@@ -3,55 +3,65 @@
 
 using namespace std;
 
-class Unit : public GameObject {
-public:
-    explicit Unit(geo2d::Point position) :position_(position) {}
+// Создаём шаблон класса Collider, только в нём будет переопределяться метод Collide 
+template <typename T>
+struct Collider : GameObject {
+    bool Collide(const GameObject& that) const final {
+        // Статически приводим тип *this к типу const T&, потому что мы знаем, 
+        // что T — наш наследник (см. ниже) 
+        return that.CollideWith(static_cast<const T&>(*this));
+    }
+};
+
+class Unit final : public Collider<Unit> { 
+public: 
+  Unit(geo2d::Point position): position_(position) {}
     geo2d::Point GetGeo() const { return position_; }
-    bool Collide(const GameObject& that) const override;
-    bool CollideWith(const Unit& that) const override;
-    bool CollideWith(const Building& that) const override;
-    bool CollideWith(const Tower& that) const override;
-    bool CollideWith(const Fence& that) const override;
+
+    bool CollideWith(const Unit& that) const override{ return false;}
+    bool CollideWith(const Building& that) const override{ return false;}
+    bool CollideWith(const Tower& that) const override{ return false;}
+    bool CollideWith(const Fence& that) const override{ return false;}
 
 private:
     geo2d::Point position_;
 };
 
-class Building : public GameObject {
+class Building : public Collider<Building>  {
 public:
     explicit Building(geo2d::Rectangle geometry) :geometry_(geometry) {}
     geo2d::Rectangle GetGeo() const { return geometry_; }
-    bool Collide(const GameObject& that) const override;
-    bool CollideWith(const Unit& that) const override;
-    bool CollideWith(const Building& that) const override;
-    bool CollideWith(const Tower& that) const override;
-    bool CollideWith(const Fence& that) const override;
+
+    bool CollideWith(const Unit& that) const override{ return false;}
+    bool CollideWith(const Building& that) const override{ return false;}
+    bool CollideWith(const Tower& that) const override{ return false;}
+    bool CollideWith(const Fence& that) const override{ return false;}
 private:
     geo2d::Rectangle geometry_;
 };
 
-class Tower : public GameObject {
+class Tower : public Collider<Tower> {
 public:
     explicit Tower(geo2d::Circle geometry) :geometry_(geometry) {}
     geo2d::Circle GetGeo() const { return geometry_; }
-    bool Collide(const GameObject& that) const override;
-    bool CollideWith(const Unit& that) const override;
-    bool CollideWith(const Building& that) const override;
-    bool CollideWith(const Tower& that) const override;
-    bool CollideWith(const Fence& that) const override;
+
+    bool CollideWith(const Unit& that) const override{ return false;}
+    bool CollideWith(const Building& that) const override{ return false;}
+    bool CollideWith(const Tower& that) const override{ return false;}
+    bool CollideWith(const Fence& that) const override{ return false;}
 private:
     geo2d::Circle geometry_;
 };
 
-class Fence : public GameObject {
+class Fence : public Collider<Fence> {
 public:
     explicit Fence(geo2d::Segment geometry) :geometry_(geometry) {}
     geo2d::Segment GetGeo() const { return geometry_; }
-    bool Collide(const GameObject& that) const override;
-    bool CollideWith(const Unit& that) const override;
-    bool CollideWith(const Building& that) const override;
-    bool CollideWith(const Tower& that) const override;
-    bool CollideWith(const Fence& that) const override;
+
+    bool CollideWith(const Unit& that) const override{ return false;}
+    bool CollideWith(const Building& that) const override{ return false;}
+    bool CollideWith(const Tower& that) const override{ return false;}
+    bool CollideWith(const Fence& that) const override{ return false;}
 private:
     geo2d::Segment geometry_;
 };
