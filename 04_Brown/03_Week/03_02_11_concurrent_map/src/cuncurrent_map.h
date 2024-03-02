875a79df9d4b94eb2237937aa96245e068209a41
pavelzep <pavelzep@gmail.com>
2024-03-02 23:22:45 +0500

    deleted:    04_Brown/03_Week/03_02_11_concurrent_map/src/concurrent_map_1 	modified:   04_Brown/03_Week/03_02_11_concurrent_map/src/cuncurrent_map.h

--- 04_Brown/03_Week/03_02_11_concurrent_map/src/cuncurrent_map.h
+++ 04_Brown/03_Week/03_02_11_concurrent_map/src/cuncurrent_map.h
@@ -8,6 +8,8 @@
 #include <utility>
 #include <algorithm>
 #include <random>
+#include <numeric>
+
 
 template <typename K, typename V, typename Hash = std::hash<K>>
 class ConcurrentMap {
@@ -15,22 +17,26 @@ public:
     using MapType = unordered_map<K, V, Hash>;
 
     struct WriteAccess {
+        lock_guard <mutex> g;
         V& ref_to_value;
     };
 
     struct ReadAccess {
+        lock_guard <mutex> g;
         const V& ref_to_value;
     };
 
     explicit ConcurrentMap(size_t _bucket_count) : bucket_count(_bucket_count) {
         for (size_t i = 0; i < bucket_count; ++i) {
-            storage.push_back(typename ConcurrentMap::MapType<K, V, Hash>());
+            storage.push_back(MapType());
             mutexes.push_back(make_unique<std::mutex>());
         }
     }
 
     WriteAccess operator[](const K& key) {
-        return WriteAccess();
+        // return { lock_guard(*mutexes[Abs(key) % bucket_count]),storage[Abs(key) % bucket_count][key] };
+        
+        return { lock_guard(*mutexes[Abs(key) % bucket_count]),storage[Abs(key) % bucket_count][key] };
     }
     ReadAccess At(const K& key) const {
         return ReadAccess();
