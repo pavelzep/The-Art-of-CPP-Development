00618ddf056c2a97f10f25a9207340d4ca72178e
pavelzep <pavelzep@gmail.com>
2024-03-02 16:26:51 +0500

    modified:   04_Brown/03_Week/03_02_11_concurrent_map/src/cuncurrent_map.h

--- 04_Brown/03_Week/03_02_11_concurrent_map/src/cuncurrent_map.h
+++ 04_Brown/03_Week/03_02_11_concurrent_map/src/cuncurrent_map.h
@@ -12,49 +12,41 @@
 template <typename K, typename V, typename Hash = std::hash<K>>
 class ConcurrentMap {
 public:
-  using MapType = unordered_map<K, V, Hash>;
+    using MapType = unordered_map<K, V, Hash>;
 
-  struct WriteAccess {
-    V& ref_to_value;
-  };
+    struct WriteAccess {
+        V& ref_to_value;
+    };
 
-  struct ReadAccess {
-    const V& ref_to_value;
-  };
+    struct ReadAccess {
+        const V& ref_to_value;
+    };
 
-  explicit ConcurrentMap(size_t bucket_count);
+    explicit ConcurrentMap(size_t _bucket_count) : bucket_count(_bucket_count) {
+        for (size_t i = 0; i < bucket_count; ++i) {
+            storage.push_back(typename ConcurrentMap::MapType<K, V, Hash>());
+            mutexes.push_back(make_unique<std::mutex>());
+        }
+    }
 
-  WriteAccess operator[](const K& key);
-  ReadAccess At(const K& key) const;
+    WriteAccess operator[](const K& key) {
+        return WriteAccess();
+    }
+    ReadAccess At(const K& key) const {
+        return ReadAccess();
+    }
 
-  bool Has(const K& key) const;
+    bool Has(const K& key) const {
+        return false;
+    }
 
-  MapType BuildOrdinaryMap() const;
+    MapType BuildOrdinaryMap() const {
+        return MapType();
+    }
 
 private:
-  Hash hasher;
+    Hash hasher;
+    vector <MapType> storage;
+    vector <unique_ptr<mutex>>mutexes;
+    size_t bucket_count;
 };
-
-template<typename K, typename V, typename Hash>
-inline ConcurrentMap<K, V, Hash>::ConcurrentMap(size_t bucket_count) {
-}
-
-template<typename K, typename V, typename Hash>
-inline typename ConcurrentMap<K, V, Hash>::WriteAccess ConcurrentMap<K, V, Hash>::operator[](const K& key) {
-    return WriteAccess();
-}
-
-template<typename K, typename V, typename Hash>
-inline typename ConcurrentMap<K, V, Hash>::ReadAccess ConcurrentMap<K, V, Hash>::At(const K& key) const {
-    return ReadAccess();
-}
-
-template<typename K, typename V, typename Hash>
-inline bool ConcurrentMap<K, V, Hash>::Has(const K& key) const {
-    return false;
-}
-
-template<typename K, typename V, typename Hash>
-inline typename ConcurrentMap<K, V, Hash>::MapType ConcurrentMap<K, V, Hash>::BuildOrdinaryMap() const {
-    return MapType();
-}
