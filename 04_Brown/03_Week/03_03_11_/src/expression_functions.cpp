d48c22853dd8ec78f5f852290a0e1743f879b3b7
pavelzep <pavelzep@gmail.com>
2024-03-18 16:52:51 +0500

    new file:   04_Brown/03_Week/03_03_11_/CMakeLists.txt 	new file:   04_Brown/03_Week/03_03_11_/readme.md 	new file:   04_Brown/03_Week/03_03_11_/src/Common.h 	new file:   04_Brown/03_Week/03_03_11_/src/expression_functions.cpp 	new file:   04_Brown/03_Week/03_03_11_/src/main.cpp 	new file:   04_Brown/03_Week/03_03_11_/src/test_runner.h

---
+++ 04_Brown/03_Week/03_03_11_/src/expression_functions.cpp
@@ -0,0 +1,11 @@
+#include "common.h"
+
+ExpressionPtr Value(int value) {
+
+}
+ExpressionPtr Sum(ExpressionPtr left, ExpressionPtr right) {
+
+}
+ExpressionPtr Product(ExpressionPtr left, ExpressionPtr right) {
+
+}
