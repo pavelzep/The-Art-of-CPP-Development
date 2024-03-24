337f0c35382829aa485609c3a56729a554489b13
pavelzep <pavelzep@gmail.com>
2024-03-24 22:52:21 +0500

    modified:   04_Brown/04_Week/04_02_05_textures/src/Solution.cpp 	modified:   04_Brown/04_Week/04_02_05_textures/src/main.cpp

--- 04_Brown/04_Week/04_02_05_textures/src/Solution.cpp
+++ 04_Brown/04_Week/04_02_05_textures/src/Solution.cpp
@@ -5,48 +5,77 @@ using namespace std;
 // Этот файл сдаётся на проверку
 // Здесь напишите реализацию необходимых классов-потомков `IShape`
 
-
+// template <typename T>
 class Figure : public IShape {
+public:
+    // std::unique_ptr<IShape> Clone() const {
+    // }
 
-    std::unique_ptr<IShape> Clone() const {
+    void SetPosition(Point position) final { this->position_ = position; }
+    Point GetPosition() const final { return position_; }
+    void SetSize(Size size) final { this->size_ = size; }
+    Size GetSize() const final { return size_; }
+    void SetTexture(std::shared_ptr<ITexture> texture) final { this->texture_ = texture; }
+    ITexture* GetTexture() const final { return texture_.get(); }
 
-    }
-
-    void SetPosition(Point position) { this->position_ = position; }
-    Point GetPosition() const { return position_; }
-    void SetSize(Size size) { this->size_ = size; }
-    Size GetSize() const { return size_; }
-
-    void SetTexture(std::shared_ptr<ITexture>) {
-    }
-    
-    ITexture* GetTexture() const {
-    }
-
-    void Draw(Image&) const {
-    }
+    // void SetType(ShapeType shape_type)  { this->shape_type_ = shape_type; }
+    // ShapeType GetType() const { return shape_type_; }
 
 private:
     Point position_;
     Size size_;
+    std::shared_ptr<ITexture> texture_;
+    // ShapeType shape_type_;
 };
 
 class Rectangle : public Figure {
 public:
+    std::unique_ptr<IShape> Clone() const final {
+        auto clone = make_unique<Rectangle>();
+        clone.get()->SetPosition(this->GetPosition());
+        clone.get()->SetSize(this->GetSize());
+        clone.get()->SetTexture(shared_ptr<ITexture>(this->GetTexture()));
+        return clone;
+    }
 
+    void Draw(Image& im) const override {
+
+        Size im_size = { static_cast<int>(im.at(0).size()), static_cast<int>(im.size()) };
+        Size t_size = GetSize();
+        Point t_offset = GetPosition();
+        
+
+
+    }
 };
 
 class Ellipse : public Figure {
 public:
+    std::unique_ptr<IShape> Clone() const final {
+        auto clone = make_unique<Ellipse>();
+        clone.get()->SetPosition(this->GetPosition());
+        clone.get()->SetSize(this->GetSize());
+        clone.get()->SetTexture(shared_ptr<ITexture>(this->GetTexture()));
+        return clone;
+    }
+
+    void Draw(Image&) const override {
+    }
 
 };
 
-// Напишите реализацию функции
+// template <typename T>
+// unique_ptr<IShape> MakeShape(T shape_type) {
+//     unique_ptr<IShape> Shape = make_unique<T>();
+//     return Shape;
+// }
+
 unique_ptr<IShape> MakeShape(ShapeType shape_type) {
     unique_ptr<IShape> Shape;
     switch (shape_type) {
         case ShapeType::Rectangle: {
             Shape = make_unique<Rectangle>();
+            // Shape.get()
 
             break;
         }
