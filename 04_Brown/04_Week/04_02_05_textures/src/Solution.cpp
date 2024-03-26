8dc6b3dfd869205d6ba9c1cfc6ab082e1b6f1caa
pavezep <pavelzep@gmail.com>
2024-03-26 22:52:01 +0500

    modified:   04_Brown/04_Week/04_02_05_textures/src/Solution.cpp

--- 04_Brown/04_Week/04_02_05_textures/src/Solution.cpp
+++ 04_Brown/04_Week/04_02_05_textures/src/Solution.cpp
@@ -5,7 +5,10 @@ using namespace std;
 // Этот файл сдаётся на проверку
 // Здесь напишите реализацию необходимых классов-потомков `IShape`
 
-// template <typename T>
+inline bool IsPointInRectangle(Point p, Size size) {
+    return ((p.x < size.width) && (p.y < size.height));
+}
+
 class Figure : public IShape {
 public:
     // std::unique_ptr<IShape> Clone() const {
@@ -20,11 +23,14 @@ public:
 
     // void SetType(ShapeType shape_type)  { this->shape_type_ = shape_type; }
     // ShapeType GetType() const { return shape_type_; }
-
 private:
     Point position_;
     Size size_;
+protected:
     std::shared_ptr<ITexture> texture_;
+    // void FillByTextures() {
+
+    // }
     // ShapeType shape_type_;
 };
 
@@ -34,17 +40,44 @@ public:
         auto clone = make_unique<Rectangle>();
         clone.get()->SetPosition(this->GetPosition());
         clone.get()->SetSize(this->GetSize());
-        clone.get()->SetTexture(shared_ptr<ITexture>(this->GetTexture()));
+        // clone.get()->SetTexture(shared_ptr<ITexture>(this->GetTexture()));
+        clone.get()->SetTexture(texture_);
         return clone;
     }
 
     void Draw(Image& im) const override {
 
         Size im_size = { static_cast<int>(im.at(0).size()), static_cast<int>(im.size()) };
-        Size t_size = GetSize();
-        Point t_offset = GetPosition();
-        
+        Size figure_size = GetSize();
+        Point figure_offset = GetPosition();
+        Image figure(figure_size.height, string(figure_size.width, '.'));
 
+        if (GetTexture()) {
+
+            Size texture_size = GetTexture()->GetSize();
+            Image texture = GetTexture()->GetImage();
+
+            for (int y = 0; y < texture_size.height; y++) {
+                // if (y <= figure_size.height) {
+                for (int x = 0; x < texture_size.width; x++) {
+                    // if (x < figure_size.width) {
+                    if (IsPointInRectangle(Point{ x, y }, figure_size)) {
+                        figure.at(y).at(x) = texture.at(y).at(x);
+                    }
+                }
+                // }
+            }
+        }
+
+        for (int y = 0; y < figure_size.height;y++) {
+            if ((0 <= y + figure_offset.y) && (y + figure_offset.y < im_size.height)) {
+                for (int x = 0; x < figure_size.width;x++) {
+                    if ((0 <= x + figure_offset.x) && (x + figure_offset.x < im_size.width)) {
+                        im.at(y + figure_offset.y).at(x + figure_offset.x) = figure.at(y).at(x);
+                    }
+                }
+            }
+        }
 
     }
 };
@@ -55,11 +88,44 @@ public:
         auto clone = make_unique<Ellipse>();
         clone.get()->SetPosition(this->GetPosition());
         clone.get()->SetSize(this->GetSize());
-        clone.get()->SetTexture(shared_ptr<ITexture>(this->GetTexture()));
+        clone.get()->SetTexture(texture_);
         return clone;
     }
 
-    void Draw(Image&) const override {
+    void Draw(Image& im) const override {
+
+        Size im_size = { static_cast<int>(im.at(0).size()), static_cast<int>(im.size()) };
+        Size figure_size = GetSize();
+        Point figure_offset = GetPosition();
+        Image figure(figure_size.height, string(figure_size.width, '.'));
+
+        if (GetTexture()) {
+
+            Size texture_size = GetTexture()->GetSize();
+            Image texture = GetTexture()->GetImage();
+
+            for (int y = 0; y < texture_size.height; y++) {
+                // if (y <= figure_size.height) {
+                for (int x = 0; x < texture_size.width; x++) {
+                    // if (x < figure_size.width) {
+                    if (IsPointInEllipse(Point{ x, y }, figure_size)) {
+                        figure.at(y).at(x) = texture.at(y).at(x);
+                    }
+                }
+                // }
+            }
+        }
+
+        for (int y = 0; y < figure_size.height;y++) {
+            if ((0 <= y + figure_offset.y) && (y + figure_offset.y < im_size.height)) {
+                for (int x = 0; x < figure_size.width;x++) {
+                    if ((0 <= x + figure_offset.x) && (x + figure_offset.x < im_size.width)) {
+                        im.at(y + figure_offset.y).at(x + figure_offset.x) = figure.at(y).at(x);
+                    }
+                }
+            }
+        }
+
     }
 
 };
