ee72aa07cc084ac401fb210ac9fd77857ab03cb2
pavezep <pavelzep@gmail.com>
2024-03-27 11:45:23 +0500

    modified:   04_Brown/04_Week/04_02_05_textures/src/Solution.cpp

--- 04_Brown/04_Week/04_02_05_textures/src/Solution.cpp
+++ 04_Brown/04_Week/04_02_05_textures/src/Solution.cpp
@@ -2,48 +2,20 @@
 
 using namespace std;
 
-// Этот файл сдаётся на проверку
-// Здесь напишите реализацию необходимых классов-потомков `IShape`
-
 inline bool IsPointInRectangle(Point p, Size size) {
     return ((p.x < size.width) && (p.y < size.height));
 }
 
+// using CheckFunction = bool(*)(Point, Size);
+
 class Figure : public IShape {
 public:
-    // std::unique_ptr<IShape> Clone() const {
-    // }
-
     void SetPosition(Point position) final { this->position_ = position; }
     Point GetPosition() const final { return position_; }
     void SetSize(Size size) final { this->size_ = size; }
     Size GetSize() const final { return size_; }
     void SetTexture(std::shared_ptr<ITexture> texture) final { this->texture_ = texture; }
     ITexture* GetTexture() const final { return texture_.get(); }
-
-    // void SetType(ShapeType shape_type)  { this->shape_type_ = shape_type; }
-    // ShapeType GetType() const { return shape_type_; }
-private:
-    Point position_;
-    Size size_;
-protected:
-    std::shared_ptr<ITexture> texture_;
-    // void FillByTextures() {
-
-    // }
-    // ShapeType shape_type_;
-};
-
-class Rectangle : public Figure {
-public:
-    std::unique_ptr<IShape> Clone() const final {
-        auto clone = make_unique<Rectangle>();
-        clone.get()->SetPosition(this->GetPosition());
-        clone.get()->SetSize(this->GetSize());
-        // clone.get()->SetTexture(shared_ptr<ITexture>(this->GetTexture()));
-        clone.get()->SetTexture(texture_);
-        return clone;
-    }
 
     void Draw(Image& im) const override {
 
@@ -53,19 +25,15 @@ public:
         Image figure(figure_size.height, string(figure_size.width, '.'));
 
         if (GetTexture()) {
-
             Size texture_size = GetTexture()->GetSize();
             Image texture = GetTexture()->GetImage();
 
             for (int y = 0; y < texture_size.height; y++) {
-                // if (y <= figure_size.height) {
                 for (int x = 0; x < texture_size.width; x++) {
-                    // if (x < figure_size.width) {
-                    if (IsPointInRectangle(Point{ x, y }, figure_size)) {
+                    if (IsPointInFigure(Point{ x, y }, figure_size)) {
                         figure.at(y).at(x) = texture.at(y).at(x);
                     }
                 }
-                // }
             }
         }
 
@@ -73,12 +41,36 @@ public:
             if ((0 <= y + figure_offset.y) && (y + figure_offset.y < im_size.height)) {
                 for (int x = 0; x < figure_size.width;x++) {
                     if ((0 <= x + figure_offset.x) && (x + figure_offset.x < im_size.width)) {
-                        im.at(y + figure_offset.y).at(x + figure_offset.x) = figure.at(y).at(x);
+                        if (IsPointInFigure(Point{ x, y }, figure_size)) {
+                            im.at(y + figure_offset.y).at(x + figure_offset.x) = figure.at(y).at(x);
+                        }
                     }
                 }
             }
         }
+    }
 
+private:
+    Point position_;
+    Size size_;
+protected:
+    std::shared_ptr<ITexture> texture_;
+    virtual bool IsPointInFigure(Point p, Size size) const = 0;
+
+};
+
+class Rectangle : public Figure {
+public:
+    std::unique_ptr<IShape> Clone() const final {
+        auto clone = make_unique<Rectangle>();
+        clone.get()->SetPosition(this->GetPosition());
+        clone.get()->SetSize(this->GetSize());
+        clone.get()->SetTexture(texture_);
+        return clone;
+    }
+
+    bool IsPointInFigure(Point p, Size size) const final {
+        return IsPointInRectangle(p, size);
     }
 };
 
@@ -92,57 +84,16 @@ public:
         return clone;
     }
 
-    void Draw(Image& im) const override {
-
-        Size im_size = { static_cast<int>(im.at(0).size()), static_cast<int>(im.size()) };
-        Size figure_size = GetSize();
-        Point figure_offset = GetPosition();
-        Image figure(figure_size.height, string(figure_size.width, '.'));
-
-        if (GetTexture()) {
-
-            Size texture_size = GetTexture()->GetSize();
-            Image texture = GetTexture()->GetImage();
-
-            for (int y = 0; y < texture_size.height; y++) {
-                // if (y <= figure_size.height) {
-                for (int x = 0; x < texture_size.width; x++) {
-                    // if (x < figure_size.width) {
-                    if (IsPointInEllipse(Point{ x, y }, figure_size)) {
-                        figure.at(y).at(x) = texture.at(y).at(x);
-                    }
-                }
-                // }
-            }
-        }
-
-        for (int y = 0; y < figure_size.height;y++) {
-            if ((0 <= y + figure_offset.y) && (y + figure_offset.y < im_size.height)) {
-                for (int x = 0; x < figure_size.width;x++) {
-                    if ((0 <= x + figure_offset.x) && (x + figure_offset.x < im_size.width)) {
-                        im.at(y + figure_offset.y).at(x + figure_offset.x) = figure.at(y).at(x);
-                    }
-                }
-            }
-        }
-
+    bool IsPointInFigure(Point p, Size size) const final {
+        return IsPointInEllipse(p, size);
     }
-
 };
-
-// template <typename T>
-// unique_ptr<IShape> MakeShape(T shape_type) {
-//     unique_ptr<IShape> Shape = make_unique<T>();
-//     return Shape;
-// }
 
 unique_ptr<IShape> MakeShape(ShapeType shape_type) {
     unique_ptr<IShape> Shape;
     switch (shape_type) {
         case ShapeType::Rectangle: {
             Shape = make_unique<Rectangle>();
-            // Shape.get()
-
             break;
         }
         case ShapeType::Ellipse: {
