2fde4b8abd45afa4ec1eb0058e0222a749eaa7aa
pavelzep <pavelzep@gmail.com>
2024-03-31 15:33:04 +0500

    modified:   04_Brown/04_Week/04_02_08_cache/src/Solution.cpp 	new file:   04_Brown/04_Week/04_02_08_cache/src/original.cpp

--- 04_Brown/04_Week/04_02_08_cache/src/Solution.cpp
+++ 04_Brown/04_Week/04_02_08_cache/src/Solution.cpp
@@ -4,22 +4,26 @@ using namespace std;
 
 class LruCache : public ICache {
 public:
-    LruCache(
-        shared_ptr<IBooksUnpacker> books_unpacker,
-        const Settings& settings
-    ) {
-        // реализуйте метод
+    LruCache(shared_ptr<IBooksUnpacker> books_unpacker, const Settings& settings)
+        :books_unpacker_(books_unpacker), settings_(settings) {
     }
 
     BookPtr GetBook(const string& book_name) override {
-        // реализуйте метод
-    }
-};
 
+        return books_unpacker_->UnpackBook(book_name);
+        
+    }
+
+private:
+    Settings settings_;
+    shared_ptr<IBooksUnpacker> books_unpacker_;
+};
 
 unique_ptr<ICache> MakeCache(
     shared_ptr<IBooksUnpacker> books_unpacker,
     const ICache::Settings& settings
 ) {
-    // реализуйте функцию
+    unique_ptr<ICache> cache;
+    cache = make_unique<LruCache>(books_unpacker, settings);
+    return cache;
 }
