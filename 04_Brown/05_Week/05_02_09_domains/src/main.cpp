718e75031715e00228d2d618d87be725346cf3ab
pavelzep <pavelzep@gmail.com>
2024-07-01 21:29:22 +0500

    modified:   04_Brown/05_Week/05_02_09_domains/CMakeLists.txt 	new file:   04_Brown/05_Week/05_02_09_domains/src/domains_new_wa.cpp 	modified:   04_Brown/05_Week/05_02_09_domains/src/main.cpp

--- 04_Brown/05_Week/05_02_09_domains/src/main.cpp
+++ 04_Brown/05_Week/05_02_09_domains/src/main.cpp
@@ -1,42 +1,93 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
-#include <string_view>
 #include <vector>
 #include <sstream>
 
+#include "test_runner.h"
+
+#define TEST
+
+#ifdef TEST
+void test_all();
+void Test1();
+void Test2();
+void Test3();
+void Test4();
+#endif
+
 using namespace std;
-
-
-bool IsSubdomain(string_view subdomain, string_view domain) {
-    auto i = subdomain.size() - 1;
-    auto j = domain.size() - 1;
-    while (i >= 0 && j >= 0) {
-        if (subdomain[i--] != domain[j--]) {
-            return false;
-        }
-    }
-    return (i < 0 && domain[j] == '.')
-        || (j < 0 && subdomain[i] == '.');
-}
-
 
 vector<string> ReadDomains(istream& in_stream = cin) {
     size_t count;
     in_stream >> count;
     in_stream.ignore();
     vector<string> domains;
+
+    domains.reserve(count);
+
     for (size_t i = 0; i < count; ++i) {
         string domain;
         getline(in_stream, domain);
-        domains.push_back(domain);
+        reverse(domain.begin(), domain.end());
+        domains.push_back(move(domain));
     }
     return domains;
 }
 
+bool isSubDomain(const string& check, const vector<string>& banned_domains) {
+
+    for (const auto& b_domain : banned_domains) {
+        if (check.find(b_domain) == 0) {
+            auto bad_size = b_domain.size();
+            auto check_size = check.size();
+            if (bad_size >= check_size)
+                return 1;
+            else if (check.at(bad_size) == '.')
+                return 1;
+    }
+}
+return 0;
+}
+
+void printResult(const vector<bool>& result, ostream& out_stream = cout) {
+    for (const auto& res : result) {
+        res ? out_stream << "Bad" : out_stream << "Good";
+        out_stream << '\n';
+    }
+}
+
+vector<bool> checkDomains(const vector<string>& banned_domains, const vector<string>& domains_to_check) {
+    vector<bool> results;
+    results.reserve(domains_to_check.size());
+
+    for (const auto& domain : domains_to_check) {
+        results.push_back(isSubDomain(domain, banned_domains));
+    }
+    return results;
+}
 
 int main() {
 
+#ifdef TEST
+    test_all();
+#else
+    const vector<string> banned_domains = ReadDomains();
+
+    const vector<string> domains_to_check = ReadDomains();
+    printResult(checkDomains(banned_domains, domains_to_check));
+#endif
+    return 0;
+}
+#ifdef TEST
+void test_all() {
+    TestRunner tr;
+    RUN_TEST(tr, Test1);
+    RUN_TEST(tr, Test2);
+    RUN_TEST(tr, Test3);
+    RUN_TEST(tr, Test4);
+}
+void Test1() {
     std::istringstream banned_domains_ss(
         "4\n"       \
         "ya.ru\n"   \
@@ -56,32 +107,52 @@ int main() {
         "ya.ya\n"
     );
 
-    // std::string str1 = banned_domains_ss.str();
-    // std::string str2 = domains_to_check_ss.str();
-
-    vector<string> banned_domains = ReadDomains(banned_domains_ss);
+    const vector<string> banned_domains = ReadDomains(banned_domains_ss);
     const vector<string> domains_to_check = ReadDomains(domains_to_check_ss);
 
-    for (string& domain : banned_domains) {
-        reverse(begin(domain), end(domain));
-    }
-    sort(begin(banned_domains), end(banned_domains));
+    ostringstream out;
+    printResult(checkDomains(banned_domains, domains_to_check), out);
+    string right_res(
+        "Bad\n"     \
+        "Bad\n"     \
+        "Bad\n"     \
+        "Bad\n"     \
+        "Bad\n"     \
+        "Good\n"    \
+        "Good\n"
+    );
+    ASSERT_EQUAL(out.str(), right_res);
 
-    //   size_t insert_pos = 0;
-    //   for (string& domain : banned_domains) {
-    //     if (insert_pos == 0 || !IsSubdomain(domain, banned_domains[insert_pos - 1])) {
-    //       swap(banned_domains[insert_pos++], domain);
-    //     }
-    //   }
-    //   banned_domains.resize(insert_pos);
-
-    //   for (const string_view domain : domains_to_check) {
-    //     if (const auto it = upper_bound(begin(banned_domains), end(banned_domains), domain);
-    //         it != begin(banned_domains) && IsSubdomain(domain, *prev(it))) {
-    //       cout << "Good" << endl;
-    //     } else {
-    //       cout << "Bad" << endl;
-    //     }
-    //   }
-    return 0;
 }
+void Test2() {
+    std::istringstream banned_domains_ss(
+        "1\n"       \
+        "com\n"
+    );
+
+    std::istringstream domains_to_check_ss(
+        "2\n"               \
+        "ya.com\n"          \
+        "rrr.scom\n"       \
+    );
+
+    const vector<string> banned_domains = ReadDomains(banned_domains_ss);
+    const vector<string> domains_to_check = ReadDomains(domains_to_check_ss);
+
+    ostringstream out;
+    printResult(checkDomains(banned_domains, domains_to_check), out);
+    string right_res(
+        "Bad\n"     \
+        "Good\n"    \
+    );
+    ASSERT_EQUAL(out.str(), right_res);
+}
+void Test3() {
+
+
+}
+void Test4() {
+
+
+}
+#endif
